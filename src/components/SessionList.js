af33ef240564a7ff2a6818715b6558da3fe57a42
Theo <theo.cranmore@gmail.com>
2019-10-06 14:47:04 +0100

    add button for sessions list view

--- src/components/SessionList.js
+++ src/components/SessionList.js
@@ -7,6 +7,9 @@ import CardContent from '@material-ui/core/CardContent';
 import {makeStyles} from '@material-ui/core/styles';
 import {Typography} from "@material-ui/core";
 import {convertDate} from '../utilities'
+import Grid from "@material-ui/core/Grid";
+import AddCircleOutline from '@material-ui/icons/AddCircleOutline';
+import update from 'immutability-helper';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import SessionDetail from "./SessionDetail";
 import Control from '../ApiControl'
@@ -56,28 +59,63 @@ class SessionsList extends React.Component {
                 this.props.apiControl.sessions.getSessions(my_data.uuid)
                     .then((data) => {
                         if (data) {
-                            this.setState({sessions: data});
+                            this.setState({
+                                sessions: data,
+                                myUUID: my_data.uuid
+                            });
                         }
                     })
             })
     }
 
     state = {
-        sessions: []
+        sessions: [],
+        myUUID: ""
+    };
+
+    emptySession = {
+        user_id: this.state.myUUID,
+        timestamp: new Date().toISOString(),
     };
 
     render() {
         return (
             <div>
                 <h1>Sessions</h1>
+                <Grid container
+                      spacing={3}
+                      justify={"center"}
+                >
+                <Grid item>
+                    <AddCircleOutline style={{cursor: "pointer", color: "darkblue", width: "280px", height: "180px", margin: "20px 20px 20px 20px"}}
+                                      onClick={() => {
+                                          let date = new Date();
+                                          let newSession = {...this.emptySession};
+                                          newSession.user_id = this.state.myUUID;
+                                          newSession.timestamp = date.toISOString();
+                                          this.setState(({
+                                              sessions: [newSession, ...this.state.sessions]
+                                          }));
+                                          this.props.apiControl.sessions.createSession(newSession).then((data) => {
+                                              const index = this.state.sessions.indexOf(newSession);
+                                              this.setState({
+                                                  sessions: update(this.state.sessions, {[index]: {uuid: {$set: data.uuid}}})
+                                              })
+
+                                          })
+                                      }
+                                      }
+                    >a</AddCircleOutline>
+                </Grid>
                 {this.state.sessions.map((session) => (
-                    <div key={session.uuid}>
+                    <Grid item key={session.uuid}>
                         <Link to={"/session/" + session.uuid} style={{ textDecoration: 'none' }}>
                             <SessionCard session={session}/>
                         </Link>
-                    </div>
+                    </Grid>
                 ))
                 }
+                </Grid>
 
             </div>
         )
