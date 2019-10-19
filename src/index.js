11845ab91dc3f296b115f5e5314f87a83a4548e5
Theo <theo.cranmore@gmail.com>
2019-10-19 18:23:56 +0100

    priorities foreign keys and list priorities api call

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Control from './ApiControl'
 import {BrowserRouter} from 'react-router-dom'
 
 
-const apiUrl = 'http://localhost:5000/api/v0.1/';
+const apiUrl = 'http://192.168.0.10:5000/api/v0.1/';
 
 ReactDOM.render((
     <BrowserRouter>
