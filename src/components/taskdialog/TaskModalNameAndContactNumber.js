9ef18fa0d0562f71fe6c5ecc1800a61a2c42d199
Theo <theo.cranmore@gmail.com>
2020-04-16 18:02:49 +0100

    fixed tasksgrid not updating properly when tasks are added or changed. some style tweaks.

--- src/components/taskdialog/TaskModalNameAndContactNumber.js
+++ src/components/taskdialog/TaskModalNameAndContactNumber.js
@@ -1,20 +1,25 @@
-import {TextFieldControlled} from "../TextFieldControlled";
 import React from "react";
+import {TextFieldControlled} from "../TextFieldControlled";
+import Grid from "@material-ui/core/Grid";
 
 export default function TaskModalNameAndContactNumber(props) {
     return (
-        <>
-        <TextFieldControlled
-            value={props.contactName}
-            label={"Contact Name"}
-            id={"contact-name"}
-            onSelect={props.onSelectName}/>
-        <TextFieldControlled
-    label={"Contact Number"}
-    id={"contact-number"}
-    value={props.contactNumber}
-    onSelect={props.onSelectContactNumber}/>
-    </>
+        <Grid container direction={"column"}>
+            <Grid item>
+            <TextFieldControlled
+                value={props.contactName}
+                label={"Contact Name"}
+                id={"contact-name"}
+                onSelect={props.onSelectName}/>
+            </Grid>
+            <Grid item>
+            <TextFieldControlled
+                label={"Contact Number"}
+                id={"contact-number"}
+                value={props.contactNumber}
+                onSelect={props.onSelectContactNumber}/>
+            </Grid>
+        </Grid>
 
     )
 }
