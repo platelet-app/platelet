06dd346cd3c470f47b84606dded8a9c2551afd64
Theo <theo.cranmore@gmail.com>
2020-12-05 23:11:53 +0000

    switch between custom dates or last num days dropdown in statistics generation scene

--- src/scenes/Statistics/StatisticsDashboard.js
+++ src/scenes/Statistics/StatisticsDashboard.js
@@ -8,36 +8,88 @@ import {getUserStatisticsRequest} from "../../redux/statistics/statisticsActions
 import {DateAndTimePicker} from "../../components/DateTimePickers";
 import Button from "@material-ui/core/Button";
 import RoleSelect from "../../components/RoleSelect";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import FormControl from "@material-ui/core/FormControl";
+import {InputLabel, MenuItem, Select} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
 
 function StatisticsDashboard() {
-    const loadingSelector = createLoadingSelector(["GET_USER_STATISTICS", "GET_PRIORITIES"]);
+    const [dateMode, setDateMode] = useState(false);
+    const classes = useStyles();
+    const loadingSelector = createLoadingSelector(["GET_USER_STATISTICS", "GET_PRIORITIES", "GET_WHOAMI"]);
     const isFetching = useSelector(state => loadingSelector(state));
     const stats = useSelector(state => state.userStatistics.statistics);
     const whoami = useSelector(state => state.whoami.user);
     const [endDateTime, setEndDateTime] = useState(new Date())
     const [startDateTime, setStartDateTime] = useState(new Date())
     const [role, setRole] = useState("coordinator");
+    const [days, setDays] = useState(3);
     const dispatch = useDispatch();
 
-    function getStatsData() {
-        if (whoami.uuid)
-            dispatch(getUserStatisticsRequest(whoami.uuid, role, startDateTime.toISOString(), endDateTime.toISOString()))
+
+    const handleDaysChange = (event) => {
+        setDays(event.target.value);
+    };
+
+
+    const picker = dateMode ? <>
+            <DateAndTimePicker
+                visible={true}
+                label={"Start date"}
+                onChange={value => setStartDateTime(value)}
+                value={startDateTime}/>
+            <DateAndTimePicker
+                visible={true}
+                label={"End date"}
+                value={endDateTime}
+                onChange={value => setEndDateTime(value)}
+            />
+        </> :
+        <div>
+            <FormControl className={classes.formControl}>
+                <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={days}
+                    onChange={handleDaysChange}
+                >
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                    <MenuItem value={7}>7</MenuItem>
+                </Select>
+            </FormControl>
+        </div>
+
+        function getStatsData() {
+            if (whoami.uuid) {
+                if (dateMode) {
+                    dispatch(getUserStatisticsRequest(whoami.uuid, role, startDateTime.toISOString(), endDateTime.toISOString()));
+                } else {
+                    const timeNow = new Date();
+                    const timeBefore = new Date();
+                    timeBefore.setDate(timeNow.getDate() - days);
+                    dispatch(getUserStatisticsRequest(whoami.uuid, role, timeBefore.toISOString(), timeNow.toISOString()));
+                }
+            }
     }
 
         return (
             <PaddedPaper>
+                <ToggleButton value={dateMode} onChange={() => setDateMode(!dateMode)}>{dateMode ? "Custom Range" : "Days"}</ToggleButton>
+                {picker}
                 <RoleSelect onSelect={(value) => setRole(value)}/>
-                <DateAndTimePicker
-                    visible={true}
-                    label={"Start date"}
-                    onChange={value => setStartDateTime(value)}
-                    value={startDateTime}/>
-                <DateAndTimePicker
-                    visible={true}
-                    label={"End date"}
-                    value={endDateTime}
-                    onChange={value => setEndDateTime(value)}
-                />
                 <Button onClick={getStatsData}>Generate</Button>
                 <TasksStatistics stats={stats}/>
             </PaddedPaper>
