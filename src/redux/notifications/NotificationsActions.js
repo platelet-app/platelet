c4cbdeb2249810959c587949ddec43aff9805b9a
Theo <theo.cranmore@gmail.com>
2020-12-08 20:39:32 +0000

    warning and error notification dispatch actions

--- src/redux/notifications/NotificationsActions.js
+++ src/redux/notifications/NotificationsActions.js
@@ -1,5 +1,15 @@
 export const DISPLAY_INFO_NOTIFICATION = 'DISPLAY_INFO_NOTIFICATION';
+export const DISPLAY_WARNING_NOTIFICATION = 'DISPLAY_WARNING_NOTIFICATION';
+export const DISPLAY_ERROR_NOTIFICATION = 'DISPLAY_ERROR_NOTIFICATION';
 
 export function displayInfoNotification(message, restoreActions, viewLink) {
     return { type: DISPLAY_INFO_NOTIFICATION, message, restoreActions, viewLink };
+}
+
+export function displayWarningNotification(message, restoreActions, viewLink) {
+    return { type: DISPLAY_WARNING_NOTIFICATION, message, restoreActions, viewLink };
+}
+
+export function displayErrorNotification(message, restoreActions, viewLink) {
+    return { type: DISPLAY_ERROR_NOTIFICATION, message, restoreActions, viewLink };
 }
--- src/redux/notifications/NotificationsReducers.js
+++ src/redux/notifications/NotificationsReducers.js
@@ -1,11 +1,26 @@
-import {DISPLAY_INFO_NOTIFICATION} from "./NotificationsActions";
+import {
+    DISPLAY_ERROR_NOTIFICATION,
+    DISPLAY_INFO_NOTIFICATION,
+    DISPLAY_WARNING_NOTIFICATION
+} from "./NotificationsActions";
 
-export function infoNotifications(state = null, action) {
+export function notification(state = null, action) {
     switch (action.type) {
-        case DISPLAY_INFO_NOTIFICATION:
+        case DISPLAY_INFO_NOTIFICATION: {
             const {message, restoreActions, viewLink} = action;
             const options = {variant: "info", autoHideDuration: 8000};
-            return { message, viewLink, restoreActions, options };
+            return {message, viewLink, restoreActions, options};
+        }
+        case DISPLAY_WARNING_NOTIFICATION: {
+            const {message, restoreActions, viewLink} = action;
+            const options = {variant: "warning", autoHideDuration: 8000};
+            return {message, viewLink, restoreActions, options};
+        }
+        case DISPLAY_ERROR_NOTIFICATION: {
+            const {message, restoreActions, viewLink} = action;
+            const options = {variant: "error", autoHideDuration: 8000};
+            return {message, viewLink, restoreActions, options};
+        }
         default:
             return state;
     }
