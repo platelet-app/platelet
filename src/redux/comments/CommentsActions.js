0baafcc358a5d03807fc3e2ea5689369d9ca9346
Theo <theo.cranmore@gmail.com>
2021-02-24 20:20:54 +0000

    comments and deliverable actions reorganise code by function

--- src/redux/comments/CommentsActions.js
+++ src/redux/comments/CommentsActions.js
@@ -5,27 +5,6 @@ export const addCommentActions = createRequestActions(addCommentPrefix);
 export const {addCommentSuccess, addCommentFailure} = createRequestFunctions(addCommentActions);
 export const ADD_COMMENT_FROM_SOCKET = "ADD_COMMENT_FROM_SOCKET";
 
-export const deleteCommentPrefix = "DELETE_COMMENT";
-export const deleteCommentActions = createRequestActions(deleteCommentPrefix);
-export const {deleteCommentSuccess, deleteCommentFailure} = createRequestFunctions(deleteCommentActions);
-export const DELETE_COMMENT_FROM_SOCKET = "DELETE_COMMENT_FROM_SOCKET";
-
-export const restoreCommentPrefix = "RESTORE_COMMENT";
-export const restoreCommentActions = createRequestActions(restoreCommentPrefix);
-export const {restoreCommentSuccess, restoreCommentFailure} = createRequestFunctions(restoreCommentActions);
-export const RESTORE_COMMENT_FROM_SOCKET = "RESTORE_COMMENT_FROM_SOCKET";
-
-export const updateCommentPrefix = "UPDATE_COMMENT";
-export const updateCommentActions = createRequestActions(updateCommentPrefix);
-export const {updateCommentSuccess, updateCommentFailure} = createRequestFunctions(updateCommentActions);
-export const UPDATE_COMMENT_FROM_SOCKET = "UPDATE_COMMENT_FROM_SOCKET";
-
-export const getCommentsPrefix = "GET_COMMENTS";
-export const getCommentsActions = createRequestActions(getCommentsPrefix);
-export const {getCommentsSuccess, getCommentsFailure, getCommentsNotFound, getCommentsForbidden} = createRequestFunctions(getCommentsActions);
-
-export const CLEAR_COMMENTS = "CLEAR_COMMENTS";
-
 export function addCommentRequest(parentUUID, payload) {
     return { type: addCommentActions.request, data: {payload: {parent_uuid: parentUUID, ...payload }} }
 }
@@ -34,21 +13,10 @@ export function addCommentFromSocket(data) {
     return { type: ADD_COMMENT_FROM_SOCKET, data }
 }
 
-export function restoreCommentRequest(commentUUID) {
-    return { type: restoreCommentActions.request, data: {commentUUID} }
-}
-
-export function restoreCommentFromSocket(data) {
-    return { type: RESTORE_COMMENT_FROM_SOCKET, data }
-}
-
-export function updateCommentRequest(commentUUID, payload) {
-    return { type: updateCommentActions.request, data: {commentUUID, payload} }
-}
-
-export function updateCommentFromSocket(data) {
-    return { type: UPDATE_COMMENT_FROM_SOCKET, data }
-}
+export const deleteCommentPrefix = "DELETE_COMMENT";
+export const deleteCommentActions = createRequestActions(deleteCommentPrefix);
+export const {deleteCommentSuccess, deleteCommentFailure} = createRequestFunctions(deleteCommentActions);
+export const DELETE_COMMENT_FROM_SOCKET = "DELETE_COMMENT_FROM_SOCKET";
 
 export function deleteCommentRequest(commentUUID) {
     return { type: deleteCommentActions.request, data: {commentUUID}}
@@ -58,9 +26,41 @@ export function deleteCommentFromSocket(data) {
     return { type: DELETE_COMMENT_FROM_SOCKET, data }
 }
 
+export const restoreCommentPrefix = "RESTORE_COMMENT";
+export const restoreCommentActions = createRequestActions(restoreCommentPrefix);
+export const {restoreCommentSuccess, restoreCommentFailure} = createRequestFunctions(restoreCommentActions);
+export const RESTORE_COMMENT_FROM_SOCKET = "RESTORE_COMMENT_FROM_SOCKET";
+
+export function restoreCommentRequest(commentUUID) {
+    return { type: restoreCommentActions.request, data: {commentUUID} }
+}
+
+export function restoreCommentFromSocket(data) {
+    return { type: RESTORE_COMMENT_FROM_SOCKET, data }
+}
+
+export const updateCommentPrefix = "UPDATE_COMMENT";
+export const updateCommentActions = createRequestActions(updateCommentPrefix);
+export const {updateCommentSuccess, updateCommentFailure} = createRequestFunctions(updateCommentActions);
+export const UPDATE_COMMENT_FROM_SOCKET = "UPDATE_COMMENT_FROM_SOCKET";
+
+export function updateCommentRequest(commentUUID, payload) {
+    return { type: updateCommentActions.request, data: {commentUUID, payload} }
+}
+
+export function updateCommentFromSocket(data) {
+    return { type: UPDATE_COMMENT_FROM_SOCKET, data }
+}
+
+export const getCommentsPrefix = "GET_COMMENTS";
+export const getCommentsActions = createRequestActions(getCommentsPrefix);
+export const {getCommentsSuccess, getCommentsFailure, getCommentsNotFound, getCommentsForbidden} = createRequestFunctions(getCommentsActions);
+
 export function getCommentsRequest(data) {
     return { type: getCommentsActions.request, data }
 }
+
+export const CLEAR_COMMENTS = "CLEAR_COMMENTS";
 
 export function clearComments() {
     return { type: CLEAR_COMMENTS }
--- src/redux/deliverables/DeliverablesActions.js
+++ src/redux/deliverables/DeliverablesActions.js
@@ -4,37 +4,38 @@ export const addDeliverablePrefix = "ADD_DELIVERABLE";
 export const addDeliverableActions = createRequestActions(addDeliverablePrefix);
 export const {addDeliverableSuccess, addDeliverableFailure, addDeliverableForbidden, addDeliverableNotFound} = createRequestFunctions(addDeliverableActions);
 
-export const deleteDeliverablePrefix = "DELETE_DELIVERABLE";
-export const deleteDeliverableActions = createRequestActions(deleteDeliverablePrefix);
-export const {deleteDeliverableSuccess, deleteDeliverableFailure, deleteDeliverableForbidden, deleteDeliverableNotFound} = createRequestFunctions(deleteDeliverableActions);
-
-export const updateDeliverablePrefix = "UPDATE_DELIVERABLE";
-export const updateDeliverableActions = createRequestActions(updateDeliverablePrefix);
-export const {updateDeliverableSuccess, updateDeliverableFailure, updateDeliverableForbidden, updateDeliverableNotFound} = createRequestFunctions(updateDeliverableActions)
-
-export const getDeliverablesPrefix = "GET_DELIVERABLES";
-export const getDeliverablesActions = createRequestActions(getDeliverablesPrefix);
-export const {getDeliverablesSuccess, getDeliverablesFailure, getDeliverablesForbidden, getDeliverablesNotFound} = createRequestFunctions(getDeliverablesActions)
-
-export const getAvailableDeliverablesPrefix = "GET_AVAILABLE_DELIVERABLES";
-export const getAvailableDeliverablesActions = createRequestActions(getAvailableDeliverablesPrefix);
-export const {getAvailableDeliverablesSuccess, getAvailableDeliverablesFailure, getAvailableDeliverablesForbidden} = createRequestFunctions(getAvailableDeliverablesActions)
-
 export function addDeliverableRequest(data) {
     return { type: addDeliverableActions.request, data }
 }
+
+export const deleteDeliverablePrefix = "DELETE_DELIVERABLE";
+export const deleteDeliverableActions = createRequestActions(deleteDeliverablePrefix);
+export const {deleteDeliverableSuccess, deleteDeliverableFailure, deleteDeliverableForbidden, deleteDeliverableNotFound} = createRequestFunctions(deleteDeliverableActions);
 
 export function deleteDeliverableRequest(data) {
     return { type: deleteDeliverableActions.request, data }
 }
 
-export function getDeliverablesRequest(data) {
-    return { type: getDeliverablesActions.request, data }
-}
+export const updateDeliverablePrefix = "UPDATE_DELIVERABLE";
+export const updateDeliverableActions = createRequestActions(updateDeliverablePrefix);
+export const {updateDeliverableSuccess, updateDeliverableFailure, updateDeliverableForbidden, updateDeliverableNotFound} = createRequestFunctions(updateDeliverableActions)
 
 export function updateDeliverableRequest(data) {
     return { type: updateDeliverableActions.request, data }
 }
+
+export const getDeliverablesPrefix = "GET_DELIVERABLES";
+export const getDeliverablesActions = createRequestActions(getDeliverablesPrefix);
+export const {getDeliverablesSuccess, getDeliverablesFailure, getDeliverablesForbidden, getDeliverablesNotFound} = createRequestFunctions(getDeliverablesActions)
+
+export function getDeliverablesRequest(data) {
+    return { type: getDeliverablesActions.request, data }
+}
+
+export const getAvailableDeliverablesPrefix = "GET_AVAILABLE_DELIVERABLES";
+export const getAvailableDeliverablesActions = createRequestActions(getAvailableDeliverablesPrefix);
+export const {getAvailableDeliverablesSuccess, getAvailableDeliverablesFailure, getAvailableDeliverablesForbidden} = createRequestFunctions(getAvailableDeliverablesActions)
+
 
 export function getAvailableDeliverablesRequest() {
     return { type: getAvailableDeliverablesActions.request }
