012c3b653d9c1476aeedcfe6e8e6c269fe21d082
Theo Cranmore <theo.cranmore@gmail.com>
2021-08-15 19:14:58 +0100

    comment author underline on hover

--- src/scenes/Comments/components/CommentAuthor.js
+++ src/scenes/Comments/components/CommentAuthor.js
@@ -2,40 +2,52 @@ import React from "react";
 import UserAvatar from "../../../components/UserAvatar";
 import { Link as RouterLink } from "react-router-dom";
 import { encodeUUID } from "../../../utilities";
-import { Tooltip } from "@material-ui/core";
+import { makeStyles, Tooltip } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
 import { Grid } from "@material-ui/core";
 import { ThemedLink } from "../../../styles/common";
 import { useTheme } from "@material-ui/core";
 
+const useStyles = makeStyles({
+    link: {
+        textDecoration: "none",
+        "&:hover": {
+            textDecoration: "underline",
+        },
+    },
+    avatar: { textDecoration: "none" },
+});
+
 const CommentAuthor = React.memo((props) => {
     const theme = useTheme();
+    const classes = useStyles();
     return (
         <Tooltip title={props.displayName}>
-            <ThemedLink
-                style={{ textDecoration: "none" }}
-                component={RouterLink}
-                to={"/user/" + encodeUUID(props.uuid)}
-            >
-                <Grid
-                    container
-                    alignItems={"center"}
-                    spacing={1}
-                    direction={"row"}
-                >
-                    <Grid item>
+            <Grid container alignItems={"center"} spacing={1} direction={"row"}>
+                <Grid item>
+                    <ThemedLink
+                        className={classes.avatar}
+                        component={RouterLink}
+                        to={"/user/" + encodeUUID(props.uuid)}
+                    >
                         <UserAvatar
                             size={theme.spacing(0.6)}
                             userUUID={props.uuid}
                             displayName={props.displayName}
                             avatarURL={props.avatarURL}
                         />
-                    </Grid>
-                    <Grid item>
-                        <Typography>{props.displayName}</Typography>
-                    </Grid>
+                    </ThemedLink>
                 </Grid>
-            </ThemedLink>
+                <Grid item>
+                    <ThemedLink
+                        className={classes.link}
+                        component={RouterLink}
+                        to={"/user/" + encodeUUID(props.uuid)}
+                    >
+                        <Typography>{props.displayName}</Typography>
+                    </ThemedLink>
+                </Grid>
+            </Grid>
         </Tooltip>
     );
 });
