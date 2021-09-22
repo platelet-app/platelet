1d72a5a4733876ae826dc967fd6708cfa0696bfe
Theo Cranmore <theo.cranmore@gmail.com>
2021-09-22 15:09:32 +0100

    visual improvements to admin add new user page

--- src/scenes/AdminControl/Components/AdminAddUser.js
+++ src/scenes/AdminControl/Components/AdminAddUser.js
@@ -1,20 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { Auth } from "aws-amplify";
-import { Grid, Button, makeStyles, TextField } from "@material-ui/core";
+import {
+    Grid,
+    Button,
+    makeStyles,
+    TextField,
+    Typography,
+} from "@material-ui/core";
 import { DataStore } from "aws-amplify";
 import * as models from "../../../models/index";
 import {
     TextFieldUncontrolled,
     TextFieldControlled,
 } from "../../../components/TextFields";
-import { showHide } from "../../../styles/common";
+import { PaddedPaper, showHide } from "../../../styles/common";
 import { useDispatch } from "react-redux";
-import { displayErrorNotification } from "../../../redux/notifications/NotificationsActions";
+import {
+    displayErrorNotification,
+    displayInfoNotification,
+} from "../../../redux/notifications/NotificationsActions";
 
 const useStyles = makeStyles({
     root: {
-        width: "50%",
+        width: "100%",
         maxWidth: 460,
+    },
+    message: {
+        height: 80,
     },
 });
 const initialState = {
@@ -29,134 +41,174 @@ function AdminAddUser() {
     //TODO: eventually want to make signup only with email address unless chosen otherwise
     const [usernameMode, setUsernameMode] = useState(true);
     const [passwordVerified, setPasswordVerified] = useState(true);
+    const [inputVerified, setInputVerified] = useState(false);
+    const [isPosting, setIsPosting] = useState(false);
+    const [message, setMessage] = useState("");
     const dispatch = useDispatch();
     const classes = useStyles();
 
-    useEffect(
-        () => setPasswordVerified(state.password === state.passwordCheck),
-        [state.passwordCheck, state.password]
-    );
+    function verifyInput() {
+        setInputVerified(
+            state.username &&
+                state.password &&
+                state.attributes.name &&
+                state.attributes.email &&
+                passwordVerified
+        );
+    }
+    useEffect(verifyInput, [state, passwordVerified]);
+
+    function verifyPassword() {
+        setPasswordVerified(state.password === state.passwordCheck);
+    }
+
+    useEffect(verifyPassword, [state.passwordCheck, state.password]);
+    useEffect(() => {
+        if (message) setMessage("");
+    }, [state]);
     async function signUp() {
         try {
-            const { user } = await Auth.signUp({
+            setIsPosting(true);
+            const { userSub } = await Auth.signUp({
                 ...state,
             });
-            //TODO: remove this after DataStore sync is fixed up
+            debugger;
+            //TODO: remove this after DataStore sync is fixed up because we want the user to only be syncd after postconfirmation trigger
             DataStore.save(
                 new models.User({
                     name: state.attributes.name,
+                    id: userSub,
                     displayName: state.attributes.name,
                     active: 1,
                     username: state.username,
                     emailAddress: state.attributes.email,
                 })
             );
-            console.log(user);
+            setState(initialState);
+            setIsPosting(false);
+            setMessage(
+                "User was signed up. They will be made available after email confirmation."
+            );
         } catch (error) {
             console.log("error signing up:", error);
+            setIsPosting(false);
             dispatch(displayErrorNotification(error.message));
         }
     }
     const { show, hide } = showHide();
     return (
-        <Grid
-            container
-            className={classes.root}
-            direction={"column"}
-            justify={"flex-start"}
-            alignItems={"top"}
-            spacing={3}
-        >
-            <Grid item>
-                <TextFieldUncontrolled
-                    className={usernameMode ? show : hide}
-                    value={state.username}
-                    fullWidth
-                    label={"Username"}
-                    id={"username"}
-                    onChange={(e) => {
-                        setState({ ...state, username: e.target.value });
-                    }}
-                />
+        <PaddedPaper>
+            <Grid
+                container
+                className={classes.root}
+                direction={"column"}
+                justify={"flex-start"}
+                alignItems={"top"}
+                spacing={3}
+            >
+                <Grid item>
+                    <Typography variant={"h5"}>Add a new user</Typography>
+                </Grid>
+                <Grid item>
+                    <TextFieldUncontrolled
+                        className={usernameMode ? show : hide}
+                        value={state.username}
+                        fullWidth
+                        label={"Username"}
+                        id={"username"}
+                        onChange={(e) => {
+                            setState({ ...state, username: e.target.value });
+                        }}
+                    />
+                </Grid>
+                <Grid item>
+                    <TextFieldUncontrolled
+                        fullWidth
+                        label={"Email address"}
+                        id={"email"}
+                        onChange={(e) =>
+                            setState({
+                                ...state,
+                                username: usernameMode
+                                    ? state.username
+                                    : e.target.value,
+                                attributes: {
+                                    ...state.attributes,
+                                    email: e.target.value,
+                                },
+                            })
+                        }
+                        value={state.attributes.email}
+                    />
+                </Grid>
+                <Grid item>
+                    <TextFieldUncontrolled
+                        fullWidth
+                        label={"Name"}
+                        id={"name"}
+                        onChange={(e) =>
+                            setState({
+                                ...state,
+                                attributes: {
+                                    ...state.attributes,
+                                    name: e.target.value,
+                                },
+                            })
+                        }
+                        value={state.attributes.name}
+                    />
+                </Grid>
+                <Grid item>
+                    <TextFieldUncontrolled
+                        value={state.password}
+                        inputProps={{
+                            type: "password",
+                        }}
+                        fullWidth
+                        label={"Password"}
+                        id={"password"}
+                        autoComplete="new-password"
+                        onChange={(e) => {
+                            setState({ ...state, password: e.target.value });
+                        }}
+                    />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        margin="dense"
+                        id={"password-confirm"}
+                        error={!passwordVerified}
+                        helperText={
+                            !passwordVerified ? "Passwords do not match" : ""
+                        }
+                        label={"Password confirm"}
+                        value={state.passwordCheck}
+                        inputProps={{
+                            type: "password",
+                        }}
+                        fullWidth
+                        autoComplete="new-password"
+                        onChange={(e) => {
+                            setState({
+                                ...state,
+                                passwordCheck: e.target.value,
+                            });
+                        }}
+                    />
+                </Grid>
+                <Grid item>
+                    <Button
+                        disabled={!inputVerified || isPosting}
+                        onClick={signUp}
+                    >
+                        Add user
+                    </Button>
+                </Grid>
+                <Grid className={classes.message} item>
+                    <Typography>{message}</Typography>
+                </Grid>
             </Grid>
-            <Grid item>
-                <TextFieldUncontrolled
-                    fullWidth
-                    label={"Email address"}
-                    id={"email"}
-                    onChange={(e) =>
-                        setState({
-                            ...state,
-                            username: usernameMode
-                                ? state.username
-                                : e.target.value,
-                            attributes: {
-                                ...state.attributes,
-                                email: e.target.value,
-                            },
-                        })
-                    }
-                    value={state.attributes.email}
-                />
-            </Grid>
-            <Grid item>
-                <TextFieldUncontrolled
-                    fullWidth
-                    label={"Name"}
-                    id={"name"}
-                    onChange={(e) =>
-                        setState({
-                            ...state,
-                            attributes: {
-                                ...state.attributes,
-                                name: e.target.value,
-                            },
-                        })
-                    }
-                    value={state.attributes.name}
-                />
-            </Grid>
-            <Grid item>
-                <TextFieldUncontrolled
-                    value={state.password}
-                    inputProps={{
-                        type: "password",
-                    }}
-                    fullWidth
-                    label={"Password"}
-                    id={"password"}
-                    autoComplete="new-password"
-                    onChange={(e) => {
-                        setState({ ...state, password: e.target.value });
-                    }}
-                />
-            </Grid>
-            <Grid item>
-                <TextField
-                    margin="dense"
-                    id={"password-confirm"}
-                    error={!passwordVerified}
-                    helperText={
-                        !passwordVerified ? "Passwords do not match" : ""
-                    }
-                    label={"Password confirm"}
-                    value={state.passwordCheck}
-                    inputProps={{
-                        type: "password",
-                    }}
-                    fullWidth
-                    autoComplete="new-password"
-                    onChange={(e) => {
-                        setState({ ...state, passwordCheck: e.target.value });
-                    }}
-                />
-            </Grid>
-            <Grid item>
-                <Button disabled={!passwordVerified} onClick={signUp}>
-                    Add user
-                </Button>
-            </Grid>
-        </Grid>
+        </PaddedPaper>
     );
 }
 
