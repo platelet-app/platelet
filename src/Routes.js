22b5f465d35dfce1c7bcc4ea5919c66533b27fee
Theo Cranmore <theo.cranmore@gmail.com>
2021-09-24 22:16:32 +0100

    missing routes

--- src/Routes.js
+++ src/Routes.js
@@ -4,55 +4,67 @@
  * You may delete this file and its occurrences from the project filesystem if you are using GatsbyJS or NextJS version
  */
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import WithLayout from 'WithLayout';
-import { Main as MainLayout, Minimal as MinimalLayout } from './layouts';
+import {Main as MainLayout, Minimal as MinimalLayout} from './layouts';
 
 import {
-  IndexView,
-  NotFound as NotFoundView,
-  NotFoundCover as NotFoundCoverView,
+    IndexView,
+    NotFound as NotFoundView,
+    NotFoundCover as NotFoundCoverView,
+    AboutPlatelet as AboutPlateletView
 } from './views';
 
 const Routes = () => {
-  return (
-    <Switch>
-      <Route
-        exact
-        path="/"
-        render={matchProps => (
-          <WithLayout
-            {...matchProps}
-            component={IndexView}
-            layout={MainLayout}
-          />
-        )}
-      />
-      <Route
-        exact
-        path="/not-found"
-        render={matchProps => (
-          <WithLayout
-            {...matchProps}
-            component={NotFoundView}
-            layout={MinimalLayout}
-          />
-        )}
-      />
-      <Route
-        exact
-        path="/not-found-cover"
-        render={matchProps => (
-          <WithLayout
-            {...matchProps}
-            component={NotFoundCoverView}
-            layout={MinimalLayout}
-          />
-        )}
-      />
-      <Redirect to="/not-found-cover" />
-    </Switch>
-  );
+    return (
+        <Switch>
+            <Route
+                exact
+                path="/"
+                render={matchProps => (
+                    <WithLayout
+                        {...matchProps}
+                        component={IndexView}
+                        layout={MainLayout}
+                    />
+                )}
+            />
+            <Route
+                exact
+                path="/about-platelet"
+                render={matchProps => (
+                    <WithLayout
+                        {...matchProps}
+                        component={AboutPlateletView}
+                        layout={MainLayout}
+                    />
+                )}
+            />
+            <Route
+                exact
+                path="/not-found"
+                render={matchProps => (
+                    <WithLayout
+                        {...matchProps}
+                        component={NotFoundView}
+                        layout={MinimalLayout}
+                    />
+                )}
+            />
+            <Route
+                exact
+                path="/not-found-cover"
+                render={matchProps => (
+                    <WithLayout
+                        {...matchProps}
+                        component={NotFoundCoverView}
+                        layout={MinimalLayout}
+                    />
+                )}
+            />
+            <Redirect to="/not-found-cover"/>
+        </Switch>
+    );
 };
 
 export default Routes;
