f5c0544873aa22ee2158fc2576b1fe7d5a1e50d7
Theo Cranmore <theo.cranmore@gmail.com>
2021-09-27 23:18:38 +0100

    locations detail gets data from DataStore

--- src/scenes/Location/LocationDetail.js
+++ src/scenes/Location/LocationDetail.js
@@ -1,47 +1,129 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import CommentsSection from "../Comments/CommentsSection";
-import {decodeUUID} from "../../utilities";
-import {PaddedPaper} from "../../styles/common";
-import {useDispatch, useSelector} from "react-redux";
-import {getLocationRequest} from "../../redux/locations/LocationsActions";
-import {createLoadingSelector, createNotFoundSelector} from "../../redux/LoadingSelectors";
+import { decodeUUID } from "../../utilities";
+import { PaddedPaper } from "../../styles/common";
+import { useDispatch, useSelector } from "react-redux";
+import { getLocationRequest } from "../../redux/locations/LocationsActions";
+import {
+    createLoadingSelector,
+    createNotFoundSelector,
+} from "../../redux/LoadingSelectors";
 import NotFound from "../../ErrorComponents/NotFound";
 import FormSkeleton from "../../SharedLoadingSkeletons/FormSkeleton";
+import { DataStore } from "aws-amplify";
+import * as models from "../../models/index";
+import { dataStoreReadyStatusSelector } from "../../redux/Selectors";
+import { displayErrorNotification } from "../../redux/notifications/NotificationsActions";
+import { Grid } from "@material-ui/core";
+import { TextFieldUncontrolled } from "../../components/TextFields";
+
+const initialLocationState = {
+    name: null,
+    contact: null,
+    telephoneNumber: null,
+    emailAddress: null,
+    ward: null,
+    line1: null,
+    line2: null,
+    line3: null,
+    town: null,
+    county: null,
+    country: null,
+    state: null,
+    postcode: null,
+    what3words: null,
+};
+
+const fields = {
+    name: "Name",
+    ward: "Ward",
+    line1: "Line 1",
+    line2: "Line 2",
+    line3: "Line 3",
+    town: "Town",
+    county: "County",
+    country: "Country",
+    state: "State",
+    postcode: "Postcode",
+    what3words: "What 3 Words",
+    contact: "Contact name",
+    emailAddress: "Contact email",
+    telephoneNumber: "Contact telephone",
+};
 
 export default function LocationDetail(props) {
     const locationUUID = decodeUUID(props.match.params.location_uuid_b62);
     const dispatch = useDispatch();
-    const location = useSelector(state => state.location.location)
-    const notFoundSelector = createNotFoundSelector(["GET_LOCATION"]);
-    const notFound = useSelector(state => notFoundSelector(state));
-    const loadingSelector = createLoadingSelector(["GET_LOCATION"]);
-    const isFetching = useSelector(state => loadingSelector(state));
+    const [isFetching, setIsFetching] = useState(false);
+    const dataStoreReadyStatus = useSelector(dataStoreReadyStatusSelector);
+    const [notFound, setNotFound] = useState(false);
+    const [location, setLocation] = useState(initialLocationState);
 
-    function componentDidMount() {
-        if (locationUUID) {
-            dispatch(getLocationRequest(locationUUID))
+    async function newLocationProfile() {
+        if (!dataStoreReadyStatus) {
+            setIsFetching(true);
+        } else {
+            try {
+                const location = await DataStore.query(
+                    models.Location,
+                    locationUUID
+                );
+                setIsFetching(false);
+                if (location) setLocation(location);
+                else setNotFound(true);
+            } catch (error) {
+                setIsFetching(false);
+                dispatch(
+                    displayErrorNotification(
+                        `Failed to get user: ${error.message}`
+                    )
+                );
+                console.log("Request failed", error);
+            }
         }
     }
-
-    useEffect(componentDidMount, [props.location.key]);
+    useEffect(
+        () => newLocationProfile(),
+        [props.location.key, dataStoreReadyStatus]
+    );
 
     if (isFetching) {
-        return (
-            <FormSkeleton/>
-        )
+        return <FormSkeleton />;
     } else if (notFound) {
-        return (
-            <NotFound>Location {locationUUID} could not be found.</NotFound>
-        )
+        return <NotFound>Location {locationUUID} could not be found.</NotFound>;
     } else {
         return (
             <React.Fragment>
                 <PaddedPaper>
-                    {location.address ? location.address.line1 : ""}
+                    <Grid
+                        container
+                        direction={"column"}
+                        justify={"flex-start"}
+                        alignItems={"flex-start"}
+                        spacing={1}
+                    >
+                        {Object.keys(fields).map((key) => {
+                            return (
+                                <Grid key={key} item>
+                                    <TextFieldUncontrolled
+                                        value={location[key]}
+                                        fullWidth
+                                        label={fields[key]}
+                                        id={key}
+                                        onChange={(e) => {
+                                            setLocation({
+                                                ...location,
+                                                [key]: e.target.value,
+                                            });
+                                        }}
+                                    />
+                                </Grid>
+                            );
+                        })}
+                    </Grid>
                 </PaddedPaper>
-                <CommentsSection parentUUID={locationUUID}/>
+                <CommentsSection parentUUID={locationUUID} />
             </React.Fragment>
-
-        )
+        );
     }
 }
