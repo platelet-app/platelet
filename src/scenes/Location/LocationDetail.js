52e12855ed7654959213486b8cb03210fd15e3da
Theo Cranmore <theo.cranmore@gmail.com>
2021-09-28 00:13:49 +0100

    visual changes to location detail

--- src/scenes/Location/LocationDetail.js
+++ src/scenes/Location/LocationDetail.js
@@ -12,10 +12,11 @@ import NotFound from "../../ErrorComponents/NotFound";
 import FormSkeleton from "../../SharedLoadingSkeletons/FormSkeleton";
 import { DataStore } from "aws-amplify";
 import * as models from "../../models/index";
-import { dataStoreReadyStatusSelector } from "../../redux/Selectors";
+import { dataStoreReadyStatusSelector, getWhoami } from "../../redux/Selectors";
 import { displayErrorNotification } from "../../redux/notifications/NotificationsActions";
-import { Grid } from "@material-ui/core";
+import { Divider, Grid, makeStyles, Typography } from "@material-ui/core";
 import { TextFieldUncontrolled } from "../../components/TextFields";
+import { EditModeToggleButton } from "../../components/EditModeToggleButton";
 
 const initialLocationState = {
     name: null,
@@ -51,13 +52,23 @@ const fields = {
     telephoneNumber: "Contact telephone",
 };
 
+const useStyles = makeStyles({
+    root: {
+        width: 600,
+    },
+});
+
 export default function LocationDetail(props) {
     const locationUUID = decodeUUID(props.match.params.location_uuid_b62);
+    const whoami = useSelector(getWhoami);
     const dispatch = useDispatch();
     const [isFetching, setIsFetching] = useState(false);
     const dataStoreReadyStatus = useSelector(dataStoreReadyStatusSelector);
     const [notFound, setNotFound] = useState(false);
     const [location, setLocation] = useState(initialLocationState);
+    const [oldState, setOldState] = useState(initialLocationState);
+    const [editMode, setEditMode] = useState(false);
+    const classes = useStyles();
 
     async function newLocationProfile() {
         if (!dataStoreReadyStatus) {
@@ -69,8 +80,12 @@ export default function LocationDetail(props) {
                     locationUUID
                 );
                 setIsFetching(false);
-                if (location) setLocation(location);
-                else setNotFound(true);
+                if (location) {
+                    setLocation(location);
+                    setOldState(location);
+                } else {
+                    setNotFound(true);
+                }
             } catch (error) {
                 setIsFetching(false);
                 dispatch(
@@ -87,6 +102,38 @@ export default function LocationDetail(props) {
         [props.location.key, dataStoreReadyStatus]
     );
 
+    let editToggle = <></>;
+    if (whoami.roles) {
+        if (whoami.roles.includes("ADMIN")) {
+            editToggle = (
+                <EditModeToggleButton
+                    tooltipDefault={"Edit this location"}
+                    value={editMode}
+                    onChange={(v) => {
+                        setEditMode(v);
+                        if (!v) setLocation(oldState);
+                    }}
+                />
+            );
+        }
+    }
+
+    const header = (
+        <Typography variant={"h4"}>
+            Directory location: {location.name}
+        </Typography>
+    );
+
+    const divider = editMode ? (
+        <></>
+    ) : (
+        <div style={{ width: "80%" }}>
+            <Grid item>
+                <Divider />
+            </Grid>
+        </div>
+    );
+
     if (isFetching) {
         return <FormSkeleton />;
     } else if (notFound) {
@@ -97,16 +144,30 @@ export default function LocationDetail(props) {
                 <PaddedPaper>
                     <Grid
                         container
-                        direction={"column"}
-                        justify={"flex-start"}
+                        direction={"row"}
+                        justify={"space-between"}
+                        alignItems={"top"}
+                        spacing={3}
+                    >
+                        <Grid item>{header}</Grid>
+                        <Grid item>{editToggle}</Grid>
+                    </Grid>
+                    <Grid
+                        container
+                        direction={"row"}
+                        justify={"space-between"}
                         alignItems={"flex-start"}
                         spacing={1}
                     >
                         {Object.keys(fields).map((key) => {
                             return (
-                                <Grid key={key} item>
+                                <Grid key={key} style={{ width: "50%" }} item>
                                     <TextFieldUncontrolled
                                         value={location[key]}
+                                        InputProps={{
+                                            readOnly: !editMode,
+                                            disableUnderline: !editMode,
+                                        }}
                                         fullWidth
                                         label={fields[key]}
                                         id={key}
@@ -117,6 +178,7 @@ export default function LocationDetail(props) {
                                             });
                                         }}
                                     />
+                                    {divider}
                                 </Grid>
                             );
                         })}
