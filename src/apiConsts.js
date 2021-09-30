f4157ae9a6c7b104332ccdb553343313492142d5
Theo Cranmore <theo.cranmore@gmail.com>
2021-09-30 22:25:36 +0100

    apiConsts.js file for useful api data

---
+++ src/apiConsts.js
@@ -0,0 +1,14 @@
+export const protectedFields = [
+    "id",
+    "_version",
+    "_lastChangedAt",
+    "updatedAt",
+    "createdAt",
+];
+
+export const userRoles = {
+    admin: "ADMIN",
+    user: "USER",
+    coordinator: "COORDINATOR",
+    rider: "RIDER",
+};
