8b16c8e8564f8dd9dcdf3183307cb39bd5a6ac3d
Theo Cranmore <theo.cranmore@gmail.com>
2021-10-01 23:44:32 +0100

    revert CommentCards styles for now

--- src/scenes/Comments/styles/CommentCards.js
+++ src/scenes/Comments/styles/CommentCards.js
@@ -8,12 +8,10 @@ export const CommentCardStyled = styled(Paper)({
     maxWidth: 850,
     minHeight: 20,
     borderRadius: "1em",
-    padding: 14,
 });
 
 export const PrivateCommentCardStyled = styled(Paper)({
     position: "relative",
-    padding: 14,
     minWidth: 220,
     maxWidth: 850,
     minHeight: 20,
@@ -34,6 +32,7 @@ export const commentStyles = makeStyles((theme) => ({
     newComment: {
         width: "100%",
         minWidth: 350,
+        padding: 14,
     },
     newCommentTextField: {
         width: "100%",
@@ -48,6 +47,7 @@ export const commentStyles = makeStyles((theme) => ({
     },
     body: {
         whiteSpace: "pre-line",
+        padding: 14,
         color: theme.palette.text.primary,
     },
     icon: {
