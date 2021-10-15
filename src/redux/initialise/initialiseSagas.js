59aa9879000eac1a7915d885340a87919de061fd
Theo Cranmore <theo.cranmore@gmail.com>
2021-10-15 01:19:27 +0100

    fix demo mode not resetting data properly on page refresh

--- src/redux/initialise/initialiseSagas.js
+++ src/redux/initialise/initialiseSagas.js
@@ -8,7 +8,10 @@ import { DataStore } from "@aws-amplify/datastore";
 
 function* initialiseApp() {
     if (process.env.REACT_APP_DEMO_MODE === "true") {
+        yield call([DataStore, DataStore.start]);
+        yield call([DataStore, DataStore.stop]);
         yield call([DataStore, DataStore.clear]);
+        yield call([DataStore, DataStore.start]);
     }
     yield put(getWhoamiRequest());
     if (
