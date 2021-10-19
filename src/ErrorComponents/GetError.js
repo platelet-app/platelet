accdf2b5c4996d402ae9d3b8c12cc5134d464223
Theo Cranmore <theo.cranmore@gmail.com>
2021-10-19 01:21:07 +0100

    error state component

---
+++ src/ErrorComponents/GetError.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { PaddedPaper } from "../styles/common";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+export default function GetError(props) {
+    return (
+        <PaddedPaper>
+            <Grid
+                container
+                spacing={1}
+                direction={"column"}
+                alignItems={"center"}
+                justifyContent={"center"}
+            >
+                <Grid item>
+                    <Typography variant={"h4"}>An error occurred.</Typography>
+                </Grid>
+                <Grid item>{props.children}</Grid>
+            </Grid>
+        </PaddedPaper>
+    );
+}
