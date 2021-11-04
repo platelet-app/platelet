2acc2dd377e194c512612c98d0430e144c51984e
Theo Cranmore <theo.cranmore@gmail.com>
2021-11-04 15:04:21 +0000

    fix add task fab showing on task dialog when it shouldn't

--- src/scenes/Task/TaskDialogCompact.js
+++ src/scenes/Task/TaskDialogCompact.js
@@ -45,6 +45,7 @@ const useStyles = makeStyles((theme) => ({
         flexGrow: 1,
         alignItems: "center",
         justify: "center",
+        zIndex: 2000,
     },
     overview: {
         marginRight: drawerWidth,
