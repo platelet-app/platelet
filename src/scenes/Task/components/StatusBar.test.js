44223d896d3554dd468e157cba73becd70cdde43
Theo Cranmore <theo.cranmore@gmail.com>
2021-11-18 23:18:14 +0000

    statusbar test copy failure

--- src/scenes/Task/components/StatusBar.test.js
+++ src/scenes/Task/components/StatusBar.test.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import StatusBar from "./StatusBar";
-import { render } from "../../../test-utils";
+import { render, testingReduxStore } from "../../../test-utils";
 import { screen, waitFor } from "@testing-library/react";
 import { tasksStatus } from "../../../apiConsts";
 import { DataStore } from "aws-amplify";
 import * as models from "../../../models/index";
 import userEvent from "@testing-library/user-event";
+import {
+    displayErrorNotification,
+    displayInfoNotification,
+} from "../../../redux/notifications/NotificationsActions";
 const utils = require("../../../utilities");
-const flushPromises = () => new Promise(setImmediate);
+const reactRedux = require("react-redux");
 
 jest.mock("../../../utilities", () => {
     return {
@@ -19,7 +24,17 @@ jest.mock("../../../utilities", () => {
     };
 });
 
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+    ...jest.requireActual("react-redux"),
+    useSelector: jest.fn(),
+    useDispatch: () => mockDispatch,
+}));
+
 describe("StatusBar", () => {
+    beforeEach(() => {
+        jest.resetModules();
+    });
     it("renders correctly", () => {
         render(<StatusBar />);
     });
@@ -48,8 +63,10 @@ describe("StatusBar", () => {
         expect(screen.getByText(/Delivered/)).toBeInTheDocument();
     });
     test("click the copy to clipboard button", async () => {
+        const timeOfCall = new Date().toISOString();
         const newTask = await DataStore.save(
             new models.Task({
+                timeOfCall,
                 status: tasksStatus.new,
             })
         );
@@ -59,7 +76,29 @@ describe("StatusBar", () => {
         userEvent.click(copyButton);
         jest.spyOn(utils, "copyTaskDataToClipboard");
         await waitFor(() =>
-            expect(utils.copyTaskDataToClipboard).toHaveBeenCalled()
+            expect(utils.copyTaskDataToClipboard).toHaveBeenCalledWith({
+                ...newTask,
+                deliverables: [],
+            })
+        );
+        jest.spyOn(reactRedux, "useDispatch");
+        await waitFor(() =>
+            expect(mockDispatch).toHaveBeenCalledWith(
+                displayInfoNotification("Copied to clipboard.")
+            )
+        );
+    });
+    it("fails to copy task data to clipboard", async () => {
+        jest.restoreAllMocks();
+        render(<StatusBar taskId={"nope"} />);
+        const copyButton = screen.getByRole("button");
+        expect(copyButton).toBeInTheDocument();
+        userEvent.click(copyButton);
+        jest.spyOn(reactRedux, "useDispatch");
+        await waitFor(() =>
+            expect(mockDispatch).toHaveBeenCalledWith(
+                displayErrorNotification("Copy failed.")
+            )
         );
     });
 });
