ee99b07fee5e048167a2199f17254c1978f9a02a
Theo Cranmore <theo.cranmore@gmail.com>
2022-01-15 18:39:13 +0000

    generateS3Link updated to work with demo images and returns empty string if it fails in production mode

--- src/amplifyUtilities.js
+++ src/amplifyUtilities.js
@@ -4,8 +4,16 @@ export async function generateS3Link(key) {
     const imgKey = key.split("/").reverse()[0];
     const imgVisibility = key.split("/")[0];
 
-    const result = await Storage.get(imgKey, {
-        level: imgVisibility,
-    });
-    return result;
+    try {
+        const result = await Storage.get(imgKey, {
+            level: imgVisibility,
+        });
+        return result;
+    } catch (err) {
+        console.log(err);
+    }
+    if (process.env.REACT_APP_DEMO_MODE === "true") {
+        return `https://${process.env.REACT_APP_DEMO_PROFILE_PICTURES_BUCKET_NAME}.s3.${process.env.REACT_APP_DEMO_PROFILE_PICTURES_BUCKET_REGION}.amazonaws.com/${key}`;
+    }
+    return "";
 }
