1c4fa16b9b387be17824595f64f435c1282140a5
Theo Cranmore <theo.cranmore@gmail.com>
2022-01-19 22:16:52 +0000

    taskgridcolumn tasks status test

--- src/scenes/Dashboard/components/TasksGridColumn.test.js
+++ src/scenes/Dashboard/components/TasksGridColumn.test.js
@@ -1,65 +1,58 @@
 import React from "react";
-import { render, screen } from "../../../test-utils";
+import { render } from "../../../test-utils";
+import { screen, waitFor } from "@testing-library/react";
 import TasksGridColumn from "./TasksGridColumn";
 import { priorities, tasksStatus } from "../../../apiConsts";
 import * as amplify from "aws-amplify";
-import { act } from "react-dom/test-utils";
-import { waitFor } from "@testing-library/dom";
+import * as models from "../../../models";
 import { mockAllIsIntersecting } from "react-intersection-observer/test-utils";
+import _ from "lodash";
 
 jest.mock("aws-amplify");
 
-const mockTasks = [
-    {
-        id: "test1",
-        status: tasksStatus.NEW,
-        createdAt: "2020-01-01T00:00:00.000Z",
-        updatedAt: "2020-01-01T00:00:00.000Z",
-        pickUpLocation: {
-            id: "testLoc1",
-            line1: "123 Fake Street",
-            ward: "Fake Ward",
-            createdAt: "2020-01-01T00:00:00.000Z",
-            updatedAt: "2020-01-01T00:00:00.000Z",
-        },
-        dropOffLocation: {
-            id: "testLoc2",
-            line1: "321 Not Fake Street",
-            ward: "Second Ward",
-            createdAt: "2020-01-01T00:00:00.000Z",
-            updatedAt: "2020-01-01T00:00:00.000Z",
-        },
-        riderResponsibility: { label: "Test Responsibility" },
-        priority: priorities.medium,
-        timeOfCall: "2020-01-01T00:00:00.000Z",
-    },
-];
-
 describe("TasksGridColumn", () => {
-    it("renders without crashing", () => {
-        amplify.DataStore.query
-            .mockResolvedValueOnce([])
-            .mockResolvedValue(mockTasks);
-        render(<TasksGridColumn taskKey={[tasksStatus.new]} />);
-    });
-    it("renders the correct number of tasks", async () => {
-        amplify.DataStore.query
-            .mockResolvedValueOnce([])
-            .mockResolvedValue(mockTasks);
+    it("renders without crashing", async () => {
+        amplify.DataStore.query.mockResolvedValueOnce([]).mockResolvedValue([]);
         amplify.DataStore.observe.mockReturnValue({
             subscribe: () => ({ unsubscribe: () => {} }),
         });
-        render(
-            <TasksGridColumn
-                title={tasksStatus.new}
-                taskKey={[tasksStatus.new]}
-            />
-        );
+        render(<TasksGridColumn taskKey={[tasksStatus.new]} />);
         await waitFor(async () => {
-            expect(amplify.DataStore.query).toHaveBeenCalledTimes(1);
+            expect(amplify.DataStore.query).toHaveBeenCalledTimes(2);
+        });
+    });
+
+    it.each`
+        taskStatus
+        ${tasksStatus.completed} | ${tasksStatus.droppedOff} | ${tasksStatus.rejected} | ${tasksStatus.cancelled}
+        ${tasksStatus.active}    | ${tasksStatus.pickedUp}
+    `("renders the tasks for each status", async ({ taskStatus }) => {
+        const mockTasks = _.range(0, 10).map(
+            (i) =>
+                new models.Task({
+                    status: taskStatus,
+                    priority: priorities.medium,
+                })
+        );
+        amplify.DataStore.query
+            .mockResolvedValueOnce([])
+            .mockResolvedValueOnce(mockTasks)
+            .mockResolvedValue([]);
+        amplify.DataStore.observe.mockReturnValue({
+            subscribe: () => ({ unsubscribe: () => {} }),
+        });
+        render(<TasksGridColumn title={taskStatus} taskKey={[taskStatus]} />);
+        await waitFor(async () => {
+            expect(amplify.DataStore.query).toHaveBeenCalledTimes(12);
         });
         mockAllIsIntersecting(true);
-        expect(screen.getByText(tasksStatus.new)).toBeInTheDocument();
-        expect(await screen.findByText(priorities.medium)).toBeInTheDocument();
+        expect(screen.getByText(taskStatus)).toBeInTheDocument();
+        const links = screen.getAllByRole("link");
+        expect(links).toHaveLength(10);
+        for (const link of links) {
+            expect(link.firstChild.className).toMatch(
+                new RegExp(`makeStyles-${taskStatus}`)
+            );
+        }
     });
 });
