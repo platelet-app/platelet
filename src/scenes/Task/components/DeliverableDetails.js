10913be803685f7da82b8790e04a6d6761f28746
Theo Cranmore <theo.cranmore@gmail.com>
2022-01-21 23:42:44 +0000

    refactored deliverable tests to work on DeliverableDetails

--- src/scenes/Task/components/DeliverableDetails.js
+++ src/scenes/Task/components/DeliverableDetails.js
@@ -88,7 +88,7 @@ function DeliverableDetails(props) {
         // TODO: this needs a debouncer
         try {
             const existing = Object.values(state).find(
-                (d) => d.deliverableType.id === value.id
+                (d) => d.deliverableType && d.deliverableType.id === value.id
             );
             if (existing) {
                 const existingDeliverable = await DataStore.query(
@@ -112,11 +112,6 @@ function DeliverableDetails(props) {
                     }));
                 }
             } else {
-                const existingTask = await DataStore.query(
-                    models.Task,
-                    props.taskId
-                );
-                if (!existingTask) throw new Error("Task does not exist");
                 const { id, ...rest } = value;
                 const deliverableType = await DataStore.query(
                     models.DeliverableType,
@@ -124,6 +119,11 @@ function DeliverableDetails(props) {
                 );
                 if (!deliverableType)
                     throw new Error("Deliverable type does not exist");
+                const existingTask = await DataStore.query(
+                    models.Task,
+                    props.taskId
+                );
+                if (!existingTask) throw new Error("Task does not exist");
                 const newDeliverable = await DataStore.save(
                     new models.Deliverable({
                         task: existingTask,
@@ -181,6 +181,7 @@ function DeliverableDetails(props) {
                     }
                     return (
                         <Stack
+                            key={deliverable.id}
                             direction={"row"}
                             justifyContent={"space-between"}
                         >
