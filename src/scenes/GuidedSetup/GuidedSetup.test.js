5281a44e739960796e7ad40d3ea5be37c493e793
Theo Cranmore <theo.cranmore@gmail.com>
2022-02-04 09:28:49 +0000

    add items test guided setup

--- src/scenes/GuidedSetup/GuidedSetup.test.js
+++ src/scenes/GuidedSetup/GuidedSetup.test.js
@@ -111,6 +111,7 @@ describe("GuidedSetup", () => {
                 telephoneNumber: "01234567890",
             },
         });
+
         amplify.DataStore.query.mockResolvedValue([]);
         amplify.DataStore.save.mockResolvedValue(mockData);
         render(<GuidedSetup />, { preloadedState });
@@ -142,6 +143,96 @@ describe("GuidedSetup", () => {
         );
         await waitFor(() =>
             expect(amplify.DataStore.query).toHaveBeenCalledTimes(6)
+        );
+    });
+
+    test("adding item data", async () => {
+        const mockData = new models.Task({
+            dropOffLocation: null,
+            pickUpLocation: null,
+            priority: null,
+            status: tasksStatus.new,
+            requesterContact: { name: "", telephoneNumber: "" },
+        });
+        const mockDeliverableType = new models.DeliverableType({
+            label: "some item",
+        });
+        const mockDeliverableType2 = new models.DeliverableType({
+            label: "another thing",
+        });
+        const mockDeliverable = new models.Deliverable({
+            deliverableType: mockDeliverableType,
+            task: mockData,
+            count: 3,
+        });
+        const mockDeliverable2 = new models.Deliverable({
+            deliverableType: mockDeliverableType2,
+            task: mockData,
+            count: 1,
+        });
+        amplify.DataStore.query
+            .mockResolvedValueOnce([mockDeliverableType, mockDeliverableType2])
+            .mockResolvedValueOnce([])
+            .mockResolvedValueOnce([])
+            .mockResolvedValueOnce(mockDeliverableType)
+            .mockResolvedValue(mockDeliverableType2);
+        amplify.DataStore.save
+            .mockResolvedValueOnce(mockData)
+            .mockResolvedValueOnce(mockDeliverable)
+            .mockResolvedValue(mockDeliverable2);
+        render(<GuidedSetup />, { preloadedState });
+        await waitFor(() =>
+            expect(amplify.DataStore.query).toHaveBeenCalledTimes(3)
+        );
+        userEvent.click(screen.getByText(/ITEMS/));
+        userEvent.click(screen.getByRole("button", { name: "Add some item" }));
+        userEvent.click(screen.getByRole("button", { name: "increment" }));
+        userEvent.click(screen.getByRole("button", { name: "increment" }));
+        userEvent.click(
+            screen.getByRole("button", { name: "Add another thing" })
+        );
+        // account for debounce
+        await new Promise((r) => setTimeout(r, 300));
+        userEvent.click(
+            screen.getByRole("button", { name: "Save to dashboard" })
+        );
+        await waitFor(() =>
+            expect(amplify.DataStore.save).toHaveBeenNthCalledWith(
+                1,
+                expect.objectContaining({
+                    ..._.omit(mockData, "id"),
+                    timeOfCall: expect.any(String),
+                })
+            )
+        );
+        await waitFor(() =>
+            expect(amplify.DataStore.query).toHaveBeenNthCalledWith(
+                4,
+                models.DeliverableType,
+                mockDeliverableType.id
+            )
+        );
+        await waitFor(() =>
+            expect(amplify.DataStore.save).toHaveBeenNthCalledWith(
+                2,
+                expect.objectContaining(_.omit(mockDeliverable, "id"))
+            )
+        );
+        await waitFor(() =>
+            expect(amplify.DataStore.query).toHaveBeenNthCalledWith(
+                5,
+                models.DeliverableType,
+                mockDeliverableType2.id
+            )
+        );
+        await waitFor(() =>
+            expect(amplify.DataStore.save).toHaveBeenNthCalledWith(
+                3,
+                expect.objectContaining(_.omit(mockDeliverable2, "id"))
+            )
+        );
+        await waitFor(() =>
+            expect(amplify.DataStore.query).toHaveBeenCalledTimes(8)
         );
     });
 
