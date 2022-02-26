c88ee352bdeec5d78c77ba62ad3d0e81d1f3ef01
Theo Cranmore <theo.cranmore@gmail.com>
2022-02-26 21:40:48 +0000

    better filtering by prev week for completed tab columns

--- src/scenes/Dashboard/utilities/getTasksAll.js
+++ src/scenes/Dashboard/utilities/getTasksAll.js
@@ -5,25 +5,49 @@ import {
     isCompletedTab,
 } from "./functions";
 import * as models from "../../../models";
+import moment from "moment";
 
 export default async function getTasksAll(keys = []) {
     const allAssignments = await DataStore.query(models.TaskAssignee);
     let tasksResult = [];
-    tasksResult = await DataStore.query(
-        models.Task,
-        (task) =>
-            task.or((task) =>
-                task
-                    // TODO: not ideal since it sometimes is one index but works for now
-                    .status("eq", keys[0])
-                    .status("eq", keys[1])
-            ),
+    if (isCompletedTab(keys)) {
+        const newMoment = moment();
+        const start = newMoment.toISOString();
+        const end = newMoment.subtract(1, "week").toISOString();
+        tasksResult = await DataStore.query(
+            models.Task,
+            (task) =>
+                task.or((task) =>
+                    task
+                        // TODO: not ideal since it sometimes is one index but works for now
+                        .status("eq", keys[0])
+                        .status("eq", keys[1])
+                        .createdAt("le", start)
+                        .createdAt("ge", end)
+                ),
 
-        {
-            sort: (s) => s.createdAt("desc"),
-            limit: isCompletedTab(keys) ? 200 : 0,
-        }
-    );
+            {
+                sort: (s) => s.createdAt("desc"),
+                limit: 200,
+            }
+        );
+    } else {
+        tasksResult = await DataStore.query(
+            models.Task,
+            (task) =>
+                task.or((task) =>
+                    task
+                        // TODO: not ideal since it sometimes is one index but works for now
+                        .status("eq", keys[0])
+                        .status("eq", keys[1])
+                ),
+
+            {
+                sort: (s) => s.createdAt("desc"),
+                limit: 0,
+            }
+        );
+    }
     if (isCompletedTab(keys)) {
         // filter tasksResult to only return tasks that were created in the last week
         tasksResult = tasksResult.filter(filterTasksToOneWeek);
