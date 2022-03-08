d14e5866f0f305fa8f28fb28663d9e67d2682054
Theo Cranmore <theo.cranmore@gmail.com>
2022-03-08 00:46:50 +0000

    update user list to work with changed DataStore behaviour

--- src/scenes/UsersList.js
+++ src/scenes/UsersList.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import UserCard from "../components/UserCard";
 import { contextDots, PaddedPaper } from "../styles/common";
-import { sortByCreatedTime } from "../utilities";
+import { convertListDataToObject, sortByCreatedTime } from "../utilities";
 import { Button, Stack } from "@mui/material";
 import { dataStoreReadyStatusSelector, getWhoami } from "../redux/Selectors";
 import { Link } from "react-router-dom";
@@ -10,13 +10,12 @@ import { DataStore } from "aws-amplify";
 import * as models from "../models/index";
 import { displayErrorNotification } from "../redux/notifications/NotificationsActions";
 import { userRoles } from "../apiConsts";
-import Skeleton from '@mui/material/Skeleton';
+import Skeleton from "@mui/material/Skeleton";
 import { TextFieldControlled } from "../components/TextFields";
 import SearchIcon from "@mui/icons-material/Search";
 import { InputAdornment } from "@mui/material";
 import makeStyles from "@mui/styles/makeStyles";
 import { matchSorter } from "match-sorter";
-
 
 const useStyles = makeStyles((theme) => {
     return {
@@ -33,22 +32,22 @@ const useStyles = makeStyles((theme) => {
     };
 });
 
-
-
 export default function UsersList(props) {
-    const usersRef = useRef([]); 
+    const usersRef = useRef([]);
     const [filteredUsers, setFilteredUsers] = useState([]);
+    const riderResponsibilities = useRef({});
     const [isFetching, setIsFetching] = useState(false);
     const whoami = useSelector(getWhoami);
     const dispatch = useDispatch();
     const dataStoreReadyStatus = useSelector(dataStoreReadyStatusSelector);
     const classes = useStyles();
 
-
     function onChangeFilterText(e) {
-        setFilteredUsers(matchSorter(usersRef.current, e.target.value,{
-          keys: ['name', 'displayName', 'riderResponsibility.label']
-        }))
+        setFilteredUsers(
+            matchSorter(usersRef.current, e.target.value, {
+                keys: ["name", "displayName", "riderResponsibility.label"],
+            })
+        );
     }
 
     async function getUsers() {
@@ -56,6 +55,8 @@ export default function UsersList(props) {
             setIsFetching(true);
         } else {
             try {
+                const resps = await DataStore.query(models.RiderResponsibility);
+                riderResponsibilities.current = convertListDataToObject(resps);
                 const users = await DataStore.query(models.User);
                 setIsFetching(false);
                 usersRef.current = users;
@@ -81,82 +82,95 @@ export default function UsersList(props) {
 
     if (isFetching) {
         return (
-           <Stack
+            <Stack
                 direction={"column"}
                 spacing={3}
                 alignItems={"flex-start"}
                 justifyContent={"center"}
             >
                 <PaddedPaper maxWidth={"800px"}>
-                    <Stack
-                        spacing={1}
-                        direction={"column"}
-                    > 
-                      { Array(5).fill(1).map( ele => { 
-                        return(
-                            <Stack
-                              spacing={2}
-                              direction={"row"}
-                              justifyContent={"flex-start"}
-                              alignItems={"center"}
-                            >
-                                <Skeleton variant="circular" width={40} height={40} />
-                                <Skeleton variant="text" width={500} height={50}/>
-                            </Stack>
-                          )
-                        })
-                      }
+                    <Stack spacing={1} direction={"column"}>
+                        {Array(5)
+                            .fill(1)
+                            .map((ele) => {
+                                return (
+                                    <Stack
+                                        spacing={2}
+                                        direction={"row"}
+                                        justifyContent={"flex-start"}
+                                        alignItems={"center"}
+                                    >
+                                        <Skeleton
+                                            variant="circular"
+                                            width={40}
+                                            height={40}
+                                        />
+                                        <Skeleton
+                                            variant="text"
+                                            width={500}
+                                            height={50}
+                                        />
+                                    </Stack>
+                                );
+                            })}
                     </Stack>
                 </PaddedPaper>
             </Stack>
-        )
+        );
     } else {
-      return (
-          <Stack
-              direction={"column"}
-              spacing={3}
-              alignItems={"flex-start"}
-              justifyContent={"center"}
-          >
-          {addButton}
-          <TextFieldControlled
-              id="tasks-filter-input"
-              variant={"standard"}
-              placeholder={"Filter users"}
-              onChange={onChangeFilterText}
-              color={"secondary"}
-              className={classes.root}
-              InputProps={{
-                  startAdornment: (
-                      <InputAdornment position="start">
-                          <SearchIcon className={classes.searchIcon} />
-                      </InputAdornment>
-                  ),
-              }}
-          />
-              <PaddedPaper maxWidth={"800px"}>
-                  <Stack
-                      spacing={1}
-                      direction={"column"}
-                      justifyContent={"center"}
-                      alignItems={"flex-start"}
-                  >
-                      {sortByCreatedTime(filteredUsers).map((user) => (
-                          <UserCard
-                              key={user.id}
-                              displayName={user.displayName}
-                              riderResponsibility = {user.riderResponsibility.label && user.riderResponsibility.label}
-                              userUUID={user.id}
-                              thumbnailKey={
-                                  user.profilePictureThumbnail
-                                      ? user.profilePictureThumbnail.key
-                                      : null
-                              }
-                          />
-                      ))}
-                  </Stack>
-              </PaddedPaper>
-          </Stack>
+        return (
+            <Stack
+                direction={"column"}
+                spacing={3}
+                alignItems={"flex-start"}
+                justifyContent={"center"}
+            >
+                {addButton}
+                <TextFieldControlled
+                    id="tasks-filter-input"
+                    variant={"standard"}
+                    placeholder={"Filter users"}
+                    onChange={onChangeFilterText}
+                    color={"secondary"}
+                    className={classes.root}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon className={classes.searchIcon} />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <PaddedPaper maxWidth={"800px"}>
+                    <Stack
+                        spacing={1}
+                        direction={"column"}
+                        justifyContent={"center"}
+                        alignItems={"flex-start"}
+                    >
+                        {sortByCreatedTime(filteredUsers).map((user) => {
+                            const respLabel = user.userRiderResponsibilityId
+                                ? riderResponsibilities.current[
+                                      user.userRiderResponsibilityId
+                                  ].label
+                                : null;
+                            return (
+                                <UserCard
+                                    key={user.id}
+                                    displayName={user.displayName}
+                                    riderResponsibility={respLabel}
+                                    userUUID={user.id}
+                                    thumbnailKey={
+                                        user.profilePictureThumbnail
+                                            ? user.profilePictureThumbnail.key
+                                            : null
+                                    }
+                                />
+                            );
+                        })}
+                    </Stack>
+                </PaddedPaper>
+            </Stack>
         );
     }
 }
