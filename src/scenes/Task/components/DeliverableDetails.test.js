bd514691a3242036d62eb060c61fa28f7b319a2f
Theo Cranmore <theo.cranmore@gmail.com>
2022-03-17 21:56:06 +0000

    deliverable details tests use datastore instead of mocks

--- src/scenes/Task/components/DeliverableDetails.test.js
+++ src/scenes/Task/components/DeliverableDetails.test.js
@@ -7,8 +7,7 @@ import * as amplify from "aws-amplify";
 import userEvent from "@testing-library/user-event";
 import { deliverableUnits } from "../../../apiConsts";
 import * as models from "../../../models";
-
-jest.mock("aws-amplify");
+import { DataStore, Predicates } from "aws-amplify";
 
 const preloadedState = { tenantId: "tenant-id" };
 
@@ -56,53 +55,73 @@ const mockData = [
     }),
 ];
 
+const unsubscribe = jest.fn();
+
 const fakeTask = new models.Task({
     tenantId: "tenant-id",
 });
-const mockDeliverables = _.range(0, 10).map((i) => {
-    return new models.Deliverable({
-        deliverableType: _.sample(mockData),
-        count: i + 1,
-        orderInGrid: i,
-        task: fakeTask,
-        unit: i === 0 ? deliverableUnits.item : _.sample(deliverableUnits),
-        tenantId: "tenant-id",
-    });
-});
 
-const unsubscribe = jest.fn();
+let mockDeliverables;
+
+async function saveMockDeliverables() {
+    await saveMockAvailableDeliverables();
+    const av = await DataStore.query(models.DeliverableType);
+    const task = await DataStore.save(fakeTask);
+    mockDeliverables = _.range(0, 10).map((i) => {
+        return new models.Deliverable({
+            deliverableType: _.sample(av),
+            count: i + 1,
+            orderInGrid: i,
+            task,
+            unit: i === 0 ? deliverableUnits.item : _.sample(deliverableUnits),
+            tenantId: "tenant-id",
+        });
+    });
+    for (const d of mockDeliverables) {
+        await DataStore.save(d);
+    }
+}
+
+async function saveMockAvailableDeliverables() {
+    for (const d of mockData) {
+        await DataStore.save(d);
+    }
+}
 
 describe("DeliverableDetails", () => {
+    afterEach(async () => {
+        jest.restoreAllMocks();
+        const dt = await DataStore.query(models.DeliverableType);
+        const ds = await DataStore.query(models.Deliverable);
+        const t = await DataStore.query(models.Task);
+        for (const item of [...dt, ...ds, ...t]) {
+            await DataStore.delete(item);
+        }
+    });
     it("renders", async () => {
-        amplify.DataStore.observe.mockReturnValue({
-            subscribe: () => ({ unsubscribe }),
-        });
-        amplify.DataStore.query.mockResolvedValue(mockData);
+        await saveMockAvailableDeliverables();
+        const querySpy = jest.spyOn(DataStore, "query");
         render(<DeliverableDetails />, { preloadedState });
         await waitFor(() => {
-            expect(amplify.DataStore.query).toHaveBeenCalledTimes(1);
+            expect(querySpy).toHaveBeenCalledTimes(1);
         });
     });
 
     it("renders available items in edit mode", async () => {
-        amplify.DataStore.observe.mockReturnValue({
-            subscribe: () => ({ unsubscribe }),
-        });
-        amplify.DataStore.query
-            .mockResolvedValueOnce([])
-            .mockResolvedValue(mockData);
+        await saveMockAvailableDeliverables();
+        const querySpy = jest.spyOn(DataStore, "query");
         render(<DeliverableDetails />, { preloadedState });
         await waitFor(() => {
-            expect(amplify.DataStore.query).toHaveBeenCalledTimes(1);
+            expect(querySpy).toHaveBeenCalledTimes(1);
         });
         userEvent.click(screen.getByRole("button", { name: "Edit" }));
         await waitFor(() => {
-            expect(amplify.DataStore.query).toHaveBeenCalledTimes(2);
+            expect(querySpy).toHaveBeenCalledTimes(2);
         });
         const items = await screen.findAllByRole("button", { name: /Add / });
         expect(items).toHaveLength(5);
         await waitFor(() => {
-            expect(amplify.DataStore.query).toHaveBeenCalled();
+            expect(querySpy).toHaveBeenCalled();
         });
         userEvent.click(screen.getByRole("link", { name: "More..." }));
         const itemsMore = await screen.findAllByRole("button", {
@@ -112,22 +131,14 @@ describe("DeliverableDetails", () => {
     });
 
     it("adds an item", async () => {
-        amplify.DataStore.observe.mockReturnValue({
-            subscribe: () => ({ unsubscribe }),
-        });
-
-        amplify.DataStore.query
-            .mockResolvedValueOnce([])
-            .mockResolvedValueOnce(mockData)
-            .mockResolvedValueOnce(mockData[0])
-            .mockResolvedValue(fakeTask);
-        amplify.DataStore.save.mockResolvedValueOnce(
-            new models.Deliverable({ deliverableType: mockData[0], count: 1 })
-        );
+        await saveMockAvailableDeliverables();
+        await DataStore.save(fakeTask);
+        const querySpy = jest.spyOn(DataStore, "query");
+        const saveSpy = jest.spyOn(DataStore, "save");
         render(<DeliverableDetails taskId={fakeTask.id} />, { preloadedState });
         userEvent.click(screen.getByRole("button", { name: "Edit" }));
         await waitFor(() => {
-            expect(amplify.DataStore.query).toHaveBeenCalledTimes(2);
+            expect(querySpy).toHaveBeenCalledTimes(2);
         });
         userEvent.click(
             screen.getByRole("button", { name: `Add ${mockData[0].label}` })
@@ -136,12 +147,12 @@ describe("DeliverableDetails", () => {
             name: "decrement",
         });
         await waitFor(() => {
-            expect(amplify.DataStore.save).toHaveBeenCalledTimes(1);
+            expect(saveSpy).toHaveBeenCalledTimes(1);
         });
         expect(decrement).toBeInTheDocument();
         expect(decrement).toBeDisabled();
         expect(screen.getByText("1")).toBeInTheDocument();
-        expect(amplify.DataStore.save).toHaveBeenCalledWith(
+        expect(saveSpy).toHaveBeenCalledWith(
             expect.objectContaining(
                 _.omit(
                     new models.Deliverable({
@@ -159,39 +170,33 @@ describe("DeliverableDetails", () => {
     });
 
     it("increments and decrements an item", async () => {
-        amplify.DataStore.observe.mockReturnValue({
-            subscribe: () => ({ unsubscribe }),
-        });
-        amplify.DataStore.query
-            .mockResolvedValueOnce([mockDeliverables[0]])
-            .mockResolvedValueOnce(mockData)
-            .mockResolvedValue(mockDeliverables[0]);
-        amplify.DataStore.save
-            .mockResolvedValueOnce({
-                ...mockDeliverables[0],
-                count: 2,
-            })
-            .mockResolvedValue({
-                ...mockDeliverables[0],
+        const task = await DataStore.save(fakeTask);
+        await saveMockAvailableDeliverables();
+        const mockDeliverable = await DataStore.save(
+            new models.Deliverable({
+                deliverableType: mockData[0],
+                task,
                 count: 1,
-            });
-
+                orderInGrid: 0,
+                label: mockData[0].label,
+                unit: mockData[0].defaultUnit,
+                tenantId: "tenant-id",
+            })
+        );
+        const querySpy = jest.spyOn(DataStore, "query");
+        const saveSpy = jest.spyOn(DataStore, "save");
         render(<DeliverableDetails taskId={fakeTask.id} />, { preloadedState });
         await waitFor(() => {
-            expect(amplify.DataStore.query).toHaveBeenCalledTimes(1);
+            expect(querySpy).toHaveBeenCalledTimes(1);
         });
         userEvent.click(screen.getByRole("button", { name: "Edit" }));
         await waitFor(() => {
-            expect(amplify.DataStore.query).toHaveBeenCalledTimes(2);
+            expect(querySpy).toHaveBeenCalledTimes(2);
         });
-        const increment = screen.getByRole("button", {
-            name: "increment",
-        });
-        expect(increment).toBeInTheDocument();
         userEvent.click(screen.getByRole("button", { name: /increment/ }));
         await waitFor(() => {
-            expect(amplify.DataStore.save).toHaveBeenCalledWith({
-                ...mockDeliverables[0],
+            expect(saveSpy).toHaveBeenCalledWith({
+                ...mockDeliverable,
                 count: 2,
             });
         });
@@ -201,8 +206,8 @@ describe("DeliverableDetails", () => {
         });
         userEvent.click(decrement);
         await waitFor(() => {
-            expect(amplify.DataStore.save).toHaveBeenCalledWith({
-                ...mockDeliverables[0],
+            expect(saveSpy).toHaveBeenCalledWith({
+                ...mockDeliverable,
                 count: 1,
             });
         });
@@ -211,75 +216,62 @@ describe("DeliverableDetails", () => {
     });
 
     it("deletes an item", async () => {
-        amplify.DataStore.observe.mockReturnValue({
-            subscribe: () => ({ unsubscribe }),
-        });
-        amplify.DataStore.query
-            .mockResolvedValueOnce([mockDeliverables[0]])
-            .mockResolvedValueOnce(mockData)
-            .mockResolvedValue(mockDeliverables[0]);
-        amplify.DataStore.delete.mockResolvedValueOnce({});
+        const task = await DataStore.save(fakeTask);
+        await saveMockAvailableDeliverables();
+        const mockDeliverable = await DataStore.save(
+            new models.Deliverable({
+                deliverableType: mockData[0],
+                task,
+                count: 1,
+                orderInGrid: 0,
+                label: mockData[0].label,
+                unit: mockData[0].defaultUnit,
+                tenantId: "tenant-id",
+            })
+        );
+        const querySpy = jest.spyOn(DataStore, "query");
+        const deleteSpy = jest.spyOn(DataStore, "delete");
         render(<DeliverableDetails taskId={fakeTask.id} />, { preloadedState });
         userEvent.click(screen.getByRole("button", { name: "Edit" }));
         await waitFor(() => {
-            expect(amplify.DataStore.query).toHaveBeenCalledTimes(2);
+            expect(querySpy).toHaveBeenCalledTimes(2);
         });
         userEvent.click(screen.getByRole("button", { name: "delete" }));
         await waitFor(() => {
-            expect(amplify.DataStore.delete).toHaveBeenCalledTimes(1);
+            expect(deleteSpy).toHaveBeenCalledTimes(1);
         });
-        expect(amplify.DataStore.delete).toHaveBeenCalledWith(
-            mockDeliverables[0]
-        );
+        expect(deleteSpy).toHaveBeenCalledWith(mockDeliverable);
         expect(screen.queryByRole("button", { name: "delete" })).toBeNull();
     });
 
     test("add then increment an item multiple times", async () => {
-        amplify.DataStore.observe.mockReturnValue({
-            subscribe: () => ({ unsubscribe }),
+        const task = await DataStore.save(fakeTask);
+        const mockDeliverable = new models.Deliverable({
+            deliverableType: mockData[0],
+            task,
+            count: 1,
+            orderInGrid: 0,
+            unit: mockData[0].defaultUnit,
+            tenantId: "tenant-id",
         });
-        amplify.DataStore.query
-            .mockResolvedValueOnce([])
-            .mockResolvedValueOnce(mockData)
-            .mockResolvedValueOnce(mockData[0])
-            .mockResolvedValueOnce(fakeTask)
-            .mockResolvedValueOnce(mockDeliverables[0])
-            .mockResolvedValueOnce(fakeTask);
-
-        amplify.DataStore.save
-            .mockResolvedValueOnce(mockDeliverables[0])
-            .mockResolvedValueOnce({
-                ...mockDeliverables[0],
-                count: mockDeliverables[0].count + 1,
-            });
-
-        render(<DeliverableDetails taskId={fakeTask.id} />, { preloadedState });
+        await saveMockAvailableDeliverables();
+        const querySpy = jest.spyOn(DataStore, "query");
+        const saveSpy = jest.spyOn(DataStore, "save");
+        render(<DeliverableDetails taskId={task.id} />, { preloadedState });
         await waitFor(() => {
-            expect(amplify.DataStore.query).toHaveBeenCalledTimes(1);
+            expect(querySpy).toHaveBeenCalledTimes(1);
         });
         userEvent.click(screen.getByRole("button", { name: "Edit" }));
         await waitFor(() => {
-            expect(amplify.DataStore.query).toHaveBeenCalledTimes(2);
+            expect(querySpy).toHaveBeenCalledTimes(2);
         });
         userEvent.click(
             screen.getByRole("button", { name: `Add ${mockData[0].label}` })
         );
         await waitFor(() => {
-            expect(amplify.DataStore.save).toHaveBeenNthCalledWith(
+            expect(saveSpy).toHaveBeenNthCalledWith(
                 1,
-                expect.objectContaining(
-                    _.omit(
-                        new models.Deliverable({
-                            deliverableType: mockData[0],
-                            task: fakeTask,
-                            count: 1,
-                            orderInGrid: 0,
-                            label: mockData[0].label,
-                            unit: mockData[0].defaultUnit,
-                        }),
-                        "id"
-                    )
-                )
+                expect.objectContaining(_.omit(mockDeliverable, "id"))
             );
         });
         const increments = screen.getAllByRole("button", {
@@ -291,49 +283,52 @@ describe("DeliverableDetails", () => {
         userEvent.click(increments[0]);
         userEvent.click(increments[0]);
         await waitFor(() => {
-            expect(amplify.DataStore.save).toHaveBeenNthCalledWith(2, {
-                ...mockDeliverables[0],
-                count: mockDeliverables[0].count + 5,
-            });
+            expect(saveSpy).toHaveBeenNthCalledWith(
+                2,
+                expect.objectContaining(
+                    _.omit(
+                        {
+                            ...mockDeliverable,
+                            count: mockDeliverable.count + 5,
+                        },
+                        "id"
+                    )
+                )
+            );
         });
-        expect(
-            screen.getByText(mockDeliverables[0].count + 5)
-        ).toBeInTheDocument();
+        expect(screen.getByText(mockDeliverable.count + 5)).toBeInTheDocument();
     });
 
     test("filter the available deliverables by tags", async () => {
+        await saveMockAvailableDeliverables();
+        const querySpy = jest.spyOn(DataStore, "query");
+        const predicateSpy = jest.spyOn(Predicates, "ALL");
         const tagsReducer = (previousValue, currentValue = []) => {
             const filtered = currentValue.filter(
                 (t) => !previousValue.includes(t)
             );
             return [...previousValue, ...filtered];
         };
-        amplify.DataStore.observe.mockReturnValue({
-            subscribe: () => ({ unsubscribe }),
-        });
-        amplify.DataStore.query
-            .mockResolvedValueOnce([])
-            .mockResolvedValue(mockData);
         const existingTags = Object.values(mockData).map(
             (deliverableType) => deliverableType.tags
         );
         const tagsUnique = existingTags.reduce(tagsReducer, []);
         render(<DeliverableDetails taskId={fakeTask.id} />, { preloadedState });
         await waitFor(() => {
-            expect(amplify.DataStore.query).toHaveBeenNthCalledWith(
-                1,
-                models.Deliverable
-            );
+            expect(querySpy).toHaveBeenNthCalledWith(1, models.Deliverable);
         });
 
         userEvent.click(screen.getByRole("button", { name: "Edit" }));
         await waitFor(() => {
-            expect(amplify.DataStore.query).toHaveBeenNthCalledWith(
+            expect(querySpy).toHaveBeenNthCalledWith(
                 2,
                 models.DeliverableType,
-                amplify.Predicates.ALL,
-                expect.objectContaining({})
+                expect.any(Function),
+                { sort: expect.any(Function) }
             );
+        });
+        await waitFor(() => {
+            expect(predicateSpy).toHaveBeenCalled();
         });
         userEvent.click(screen.getByText("More..."));
         for (const tag of tagsUnique) {
@@ -367,30 +362,33 @@ describe("DeliverableDetails", () => {
     });
 
     test("change the unit", async () => {
-        amplify.DataStore.observe.mockReturnValue({
-            subscribe: () => ({ unsubscribe }),
-        });
-        amplify.DataStore.query
-            .mockResolvedValueOnce([mockDeliverables[0]])
-            .mockResolvedValueOnce(mockData)
-            .mockResolvedValue(mockDeliverables[0]);
-        amplify.DataStore.save.mockResolvedValue({
-            ...mockDeliverables[0],
-            unit: deliverableUnits.none,
-        });
+        const task = await DataStore.save(fakeTask);
+        const mockDeliverable = await DataStore.save(
+            new models.Deliverable({
+                deliverableType: mockData[0],
+                task,
+                count: 1,
+                orderInGrid: 0,
+                unit: mockData[0].defaultUnit,
+                tenantId: "tenant-id",
+            })
+        );
+        await saveMockAvailableDeliverables();
+        const querySpy = jest.spyOn(DataStore, "query");
+        const saveSpy = jest.spyOn(DataStore, "save");
         render(<DeliverableDetails taskId={fakeTask.id} />, { preloadedState });
         userEvent.click(screen.getByRole("button", { name: "Edit" }));
         await waitFor(() => {
-            expect(amplify.DataStore.query).toHaveBeenCalledTimes(2);
+            expect(querySpy).toHaveBeenCalledTimes(2);
         });
         userEvent.click(
             screen.getByRole("button", {
-                name: `${mockDeliverables[0].unit}. Click to change`,
+                name: `${mockDeliverable.unit}. Click to change`,
             })
         );
         userEvent.click(
             screen.getByRole("button", {
-                name: mockDeliverables[0].unit,
+                name: mockDeliverable.unit,
             })
         );
         const unitMenuItems = await screen.findAllByRole("option");
@@ -399,10 +397,10 @@ describe("DeliverableDetails", () => {
         );
         userEvent.click(unitMenuItems[0]);
         await waitFor(() => {
-            expect(amplify.DataStore.save).toHaveBeenCalledTimes(1);
+            expect(saveSpy).toHaveBeenCalledTimes(1);
         });
-        expect(amplify.DataStore.save).toHaveBeenCalledWith({
-            ...mockDeliverables[0],
+        expect(saveSpy).toHaveBeenCalledWith({
+            ...mockDeliverable,
             unit: deliverableUnits.none,
         });
         const unitButtonUpdated = await screen.findByRole("button", {
