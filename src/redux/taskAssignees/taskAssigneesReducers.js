03f561a0f0a3b5672f482a3c9fd30163957f1059
Theo Cranmore <theo.cranmore@gmail.com>
2022-03-24 12:19:17 +0000

    unneeded console.log

--- src/redux/taskAssignees/taskAssigneesReducers.js
+++ src/redux/taskAssignees/taskAssigneesReducers.js
@@ -5,7 +5,6 @@ const initialState = { items: [], isSynced: false, ready: false };
 export function taskAssigneesReducer(state = initialState, action) {
     switch (action.type) {
         case actions.SET_TASK_ASSIGNEES:
-            console.log(action.taskAssignees);
             return action.taskAssignees;
         default:
             return state;
