d9a505d29a70ccbf42a558629e90955518575a38
Theo Cranmore <theo.cranmore@gmail.com>
2022-03-31 14:30:41 +0100

    use chips instead of radio buttons for priority select

--- src/scenes/Task/components/PrioritySelect.js
+++ src/scenes/Task/components/PrioritySelect.js
@@ -1,53 +1,29 @@
 import React, { useEffect, useState } from "react";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-import RadioGroup from "@mui/material/RadioGroup";
-import Radio from "@mui/material/Radio";
 import PropTypes from "prop-types";
-import makeStyles from "@mui/styles/makeStyles";
-import Typography from "@mui/material/Typography";
 import { priorities } from "../../../apiConsts";
-
-const useStyles = makeStyles({
-    label: {
-        fontSize: 14,
-    },
-});
+import { Chip, Stack } from "@mui/material";
 
 function PrioritySelect(props) {
-    const classes = useStyles();
     const [state, setState] = useState(null);
 
-    const handleChange = (event) => {
-        setState(event.target.value);
-        props.onSelect(event.target.value);
+    const handleChange = (value) => {
+        setState(value);
+        props.onSelect(value);
     };
 
     useEffect(() => setState(props.priority), [props.priority]);
 
     return (
-        <FormControl component="fieldset">
-            <RadioGroup
-                row
-                aria-label="priority"
-                name="priority"
-                value={state}
-                onChange={handleChange}
-            >
-                {Object.values(priorities).map((priority) => (
-                    <FormControlLabel
-                        key={priority}
-                        value={priority}
-                        control={<Radio />}
-                        label={
-                            <Typography className={classes.label}>
-                                {priority}
-                            </Typography>
-                        }
-                    />
-                ))}
-            </RadioGroup>
-        </FormControl>
+        <Stack direction="row" spacing={2}>
+            {Object.values(priorities).map((priority) => (
+                <Chip
+                    key={priority}
+                    label={priority}
+                    onClick={() => handleChange(priority)}
+                    color={state === priority ? "primary" : "default"}
+                />
+            ))}
+        </Stack>
     );
 }
 
