8d1bf3b62c94965a90cd56770098242bfe2b90df
Theo Cranmore <theo.cranmore@gmail.com>
2022-04-15 16:10:36 +0100

    extraneous console.log

--- src/redux/taskAssignees/taskAssigneesReducers.js
+++ src/redux/taskAssignees/taskAssigneesReducers.js
@@ -12,12 +12,6 @@ export function taskAssigneesReducer(state = initialState, action) {
                 items: [...state.items, action.newTaskAssignee],
             };
         case actions.DELETE_TASK_ASSIGNEE:
-            console.log({
-                ...state,
-                items: state.items.filter(
-                    (a) => a.id !== action.deletedTaskAssignee.id
-                ),
-            });
             return {
                 ...state,
                 items: state.items.filter(
