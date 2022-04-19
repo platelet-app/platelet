e4da94b039fa74d24777e472660684c745a68920
Chris McNulty <christopher.mcnulty891@gmail.com>
2022-04-19 14:29:14 +0100

    GH-43 app update notification

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import ReactNotification from "react-notifications-component";
 import "react-notifications-component/dist/theme.css";
@@ -27,8 +26,3 @@ ReactDOM.render(
     </Provider>,
     document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
