a52c04785b9269ad0d14fd74d5ab9ec3779518e7
Theo Cranmore <theo.cranmore@gmail.com>
2022-04-21 22:34:49 +0100

    profile pic lambda use env variables and custom S3 policy

--- amplify/backend/function/plateletImageResize/src/index.py
+++ amplify/backend/function/plateletImageResize/src/index.py
@@ -12,19 +12,26 @@ def resize_image(picture_file_path, crop_dimensions=None):
     if crop_dimensions:
         image = image.crop(crop_dimensions)
 
-    # TODO: make this configurable
-    image = image.resize((300, 300))
+    widthGet = os.environ.get('RESIZE_WIDTH')
+    heightGet = os.environ.get('RESIZE_HEIGHT')
+    width = int(widthGet)
+    height = int(heightGet)
+    image = image.resize((width, height))
 
     # save and convert to jpg here
     cropped_filename = os.path.join(os.path.dirname(picture_file_path), "{}_cropped.jpg".format(picture_file_path))
     thumbnail_filename = os.path.join(os.path.dirname(picture_file_path), "{}_thumbnail.jpg".format(picture_file_path))
     image.save(cropped_filename)
-    image = image.resize((128, 128))
+    thumbnailWidthGet = os.environ.get('THUMBNAIL_WIDTH')
+    thumbnailHeightGet = os.environ.get('THUMBNAIL_HEIGHT')
+    thumbnailWidth = int(thumbnailWidthGet)
+    thumbnailHeight = int(thumbnailHeightGet)
+    image = image.resize((thumbnailWidth, thumbnailHeight))
     image.save(thumbnail_filename)
     return (cropped_filename, thumbnail_filename)
 
 def handler(event, context):
-  amplify_storage_bucket_name = os.environ.get('AMPLIFY_STORAGE_BUCKET_NAME')
+  amplify_storage_bucket_name = os.environ.get('STORAGE_PLATELETSTORAGE_BUCKETNAME')
   print(os.environ)
   for record in event['Records']:
       bucket = record['s3']['bucket']['name']
