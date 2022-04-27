eff40a53e933ce322ef6d3687e5ba73f676e9c11
Theo Cranmore <theo.cranmore@gmail.com>
2022-04-27 19:36:42 +0100

    cypress env variable to delay between commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -71,6 +71,29 @@ for (const model of Object.values(models)) {
     }
 }
 
+const COMMAND_DELAY = Cypress.env("COMMAND_DELAY") || 0;
+if (COMMAND_DELAY > 0) {
+    for (const command of [
+        "visit",
+        "click",
+        "trigger",
+        "type",
+        "clear",
+        "reload",
+        "contains",
+    ]) {
+        Cypress.Commands.overwrite(command, (originalFn, ...args) => {
+            const origVal = originalFn(...args);
+
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(origVal);
+                }, COMMAND_DELAY);
+            });
+        });
+    }
+}
+
 DataStore.configure({
     syncExpressions: [
         ...modelsToSync.map((model) =>
