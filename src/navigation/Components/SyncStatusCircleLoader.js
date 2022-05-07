89b125ca3749a79856d7d39181f916a3aaa44ef3
Theo Cranmore <theo.cranmore@gmail.com>
2022-05-08 00:35:09 +0100

    sync circle style improvements

--- src/navigation/Components/SyncStatusCircleLoader.js
+++ src/navigation/Components/SyncStatusCircleLoader.js
@@ -1,15 +1,19 @@
 import React, { useEffect } from "react";
+import Tooltip from "@mui/material/Tooltip";
 import CircularProgress from "@mui/material/CircularProgress";
 import { useSelector } from "react-redux";
 import {
     dataStoreModelSyncedStatusSelector,
     networkStatusSelector,
 } from "../../redux/Selectors";
+import { Box } from "@mui/material";
 
 function SyncStatusCircleLoader() {
     const modelSyncStatus = useSelector(dataStoreModelSyncedStatusSelector);
     const [progress, setProgress] = React.useState(0);
     const [completed, setCompleted] = React.useState(false);
+    const [loadingColor, setLoadingColor] = React.useState("orange");
+    const [tooltip, setTooltip] = React.useState("Syncing data...");
     const dataStoreNetworkStatus = useSelector(networkStatusSelector);
 
     useEffect(() => {
@@ -23,7 +27,9 @@ function SyncStatusCircleLoader() {
 
     useEffect(() => {
         if (Math.round(progress) === 100) {
-            setTimeout(() => setCompleted(true), 1000);
+            setLoadingColor("lightgreen");
+            setTooltip("Data synced successfully");
+            setTimeout(() => setCompleted(true), 2000);
         }
     }, [progress]);
 
@@ -32,7 +38,34 @@ function SyncStatusCircleLoader() {
     if (completed || !dataStoreNetworkStatus) {
         return <></>;
     } else {
-        return <CircularProgress variant="determinate" value={progress} />;
+        return (
+            <Tooltip title={tooltip}>
+                <Box sx={{ display: "grid" }}>
+                    <CircularProgress
+                        size={40}
+                        variant="determinate"
+                        value={progress}
+                        sx={{
+                            zIndex: 2,
+                            gridColumn: 1,
+                            gridRow: 1,
+                            color: loadingColor,
+                        }}
+                    />
+                    {Math.round(progress) !== 100 && (
+                        <CircularProgress
+                            size={40}
+                            sx={{
+                                gridColumn: 1,
+                                gridRow: 1,
+                                opacity: 0.5,
+                                zIndex: 1,
+                            }}
+                        />
+                    )}
+                </Box>
+            </Tooltip>
+        );
     }
 }
 
