09622734acc48df06f21e96854ecd9e0648641d2
Theo Cranmore <theo.cranmore@gmail.com>
2022-06-03 19:33:24 +0100

    missing generate comments function

---
+++ src/scenes/Dashboard/utilities/generateMultipleTaskComments.js
@@ -0,0 +1,18 @@
+import { commentVisibility } from "../../../apiConsts";
+import * as models from "../../../models";
+
+async function generateMultipleTaskComments(selectedItems, body, author) {
+    if (!selectedItems || !body || !author) return;
+    return await Promise.all(
+        Object.values(selectedItems).map(async (item) => {
+            return new models.Comment({
+                body: body,
+                parentId: item.id,
+                visibility: commentVisibility.everyone,
+                author: author,
+            });
+        })
+    );
+}
+
+export default generateMultipleTaskComments;
