f74a60896fc71008d6634b6f7bfe26ff7c93a5cd
Theo Cranmore <theo.cranmore@gmail.com>
2022-06-06 16:52:40 +0100

    check selecteditems not empty in generate comments

--- src/scenes/Dashboard/utilities/generateMultipleTaskComments.js
+++ src/scenes/Dashboard/utilities/generateMultipleTaskComments.js
@@ -1,8 +1,9 @@
+import _ from "lodash";
 import { commentVisibility } from "../../../apiConsts";
 import * as models from "../../../models";
 
 async function generateMultipleTaskComments(selectedItems, body, author) {
-    if (!selectedItems || !body || !author) return;
+    if (!selectedItems || _.isEmpty(selectedItems) || !body || !author) return;
     return await Promise.all(
         Object.values(selectedItems).map(async (item) => {
             return new models.Comment({
