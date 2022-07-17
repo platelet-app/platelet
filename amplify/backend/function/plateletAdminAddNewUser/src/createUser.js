79070241fd3d478989e41880027cf8a0d8642509
Theo Cranmore <theo.cranmore@gmail.com>
2022-07-17 21:28:16 +0100

    clean up mutations in lambda function

--- amplify/backend/function/plateletAdminAddNewUser/src/createUser.js
+++ amplify/backend/function/plateletAdminAddNewUser/src/createUser.js
@@ -11,97 +11,13 @@ exports.createUser = gql`
             cognitoId
             tenantId
             contact {
-                name
-                telephoneNumber
-                mobileNumber
                 emailAddress
-                ward
-                line1
-                line2
-                line3
-                town
-                county
-                state
-                country
-                postcode
-                what3words
             }
             displayName
             name
             roles
             dateOfBirth
             riderResponsibility
-            profilePictureURL
-            profilePictureThumbnailURL
-            profilePicture {
-                bucket
-                key
-                region
-            }
-            profilePictureThumbnail {
-                bucket
-                key
-                region
-            }
-            comments {
-                items {
-                    id
-                    parentId
-                    tenantId
-                    body
-                    visibility
-                    createdAt
-                    updatedAt
-                    _version
-                    _deleted
-                    _lastChangedAt
-                    userCommentsId
-                    owner
-                }
-                nextToken
-                startedAt
-            }
-            assignments {
-                items {
-                    id
-                    tenantId
-                    taskId
-                    assigneeId
-                    role
-                    createdAt
-                    updatedAt
-                    _version
-                    _deleted
-                    _lastChangedAt
-                }
-                nextToken
-                startedAt
-            }
-            createdTasks {
-                items {
-                    id
-                    tenantId
-                    timeOfCall
-                    timePickedUp
-                    timeDroppedOff
-                    timeCancelled
-                    timeRejected
-                    timeRiderHome
-                    pickUpLocationId
-                    dropOffLocationId
-                    riderResponsibility
-                    priority
-                    status
-                    createdAt
-                    updatedAt
-                    _version
-                    _deleted
-                    _lastChangedAt
-                    userCreatedTasksId
-                }
-                nextToken
-                startedAt
-            }
             disabled
             createdAt
             updatedAt
