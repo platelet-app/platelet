651895497437a71b9aea06338e21506121faa8bd
Theo Cranmore <theo.cranmore@gmail.com>
2022-08-26 16:58:43 +0100

    plateletSendUserFeedback lambda function

---
+++ amplify/backend/function/plateletSendUserFeedback/src/index.js
@@ -0,0 +1,20 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+};
