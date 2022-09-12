078edd57b18169d278a07a3727d8fa98a5f846c9
Theo Cranmore <theo.cranmore@gmail.com>
2022-09-12 22:54:59 +0100

    convert apiConsts to typescript

---
+++ src/apiConsts.ts
@@ -0,0 +1,63 @@
+export const protectedFields = [
+    "id",
+    "_version",
+    "_lastChangedAt",
+    "_deleted",
+    "updatedAt",
+    "createdAt",
+    "tenantId",
+];
+
+export enum deliverableIcons {
+    bug = "BUG",
+    child = "CHILD",
+    document = "DOCUMENT",
+    equipment = "EQUIPMENT",
+    other = "OTHER",
+}
+
+export enum userRoles {
+    admin = "ADMIN",
+    user = "USER",
+    coordinator = "COORDINATOR",
+    rider = "RIDER",
+}
+
+export type UserRole = "ADMIN" | "RIDER" | "USER" | "COORDINATOR";
+
+export enum commentVisibility {
+    everyone = "EVERYONE",
+    me = "ME",
+}
+
+export enum tasksStatus {
+    new = "NEW",
+    active = "ACTIVE",
+    pickedUp = "PICKED_UP",
+    droppedOff = "DROPPED_OFF",
+    rejected = "REJECTED",
+    cancelled = "CANCELLED",
+    abandoned = "ABANDONED",
+    completed = "COMPLETED",
+}
+
+export enum priorities {
+    high = "HIGH",
+    medium = "MEDIUM",
+    low = "LOW",
+}
+
+export enum deliverableUnits {
+    none = "NONE",
+    litre = "LITRE",
+    millilitre = "MILLILITRE",
+    gram = "GRAM",
+    item = "ITEM",
+    box = "BOX",
+}
+
+export enum S3ObjectAccessLevels {
+    protected = "protected",
+    public = "public",
+    private = "private",
+}
