cd56b7180253eae581fbb30a613c306f2733595b
Theo Cranmore <theo.cranmore@gmail.com>
2022-10-15 17:23:56 +0100

    basic datastore conflict handler for Task plus tests

---
+++ src/redux/whoami/dataStoreConflictHandler.ts
@@ -0,0 +1,47 @@
+import { DISCARD } from "@aws-amplify/datastore";
+import { SyncConflict, PersistentModel } from "@aws-amplify/datastore";
+import * as models from "../../models";
+import determineTaskStatus from "../../utilities/determineTaskStatus";
+
+const dataStoreConflictHandler = async (
+    conflict: SyncConflict
+): Promise<symbol | PersistentModel> => {
+    const { modelConstructor, localModel, remoteModel } = conflict;
+    console.log(
+        "DataStore has found a conflict",
+        modelConstructor,
+        remoteModel,
+        localModel
+    );
+    if (modelConstructor === models.Task) {
+        let newModel = modelConstructor.copyOf(remoteModel, (task) => {
+            task.timePickedUp =
+                remoteModel.timePickedUp || localModel.timePickedUp;
+            task.timeDroppedOff =
+                remoteModel.timeDroppedOff || localModel.timeDroppedOff;
+            task.timeRiderHome =
+                remoteModel.timeRiderHome || localModel.timeRiderHome;
+            task.timeCancelled =
+                remoteModel.timeCancelled || localModel.timeCancelled;
+            task.timeRejected =
+                remoteModel.timeRejected || localModel.timeRejected;
+            task.timePickedUpSenderName =
+                remoteModel.timePickedUpSenderName ||
+                localModel.timePickedUpSenderName;
+            task.timeDroppedOffRecipientName =
+                remoteModel.timeDroppedOffRecipientName ||
+                localModel.timeDroppedOffRecipientName;
+        });
+        console.log("Resolved task conflict result:", newModel);
+        const status = await determineTaskStatus(newModel);
+        console.log("Updating task status to", status);
+        newModel = modelConstructor.copyOf(newModel, (task) => {
+            task.status = status;
+        });
+        const { createdAt, updatedAt, ...rest } = newModel;
+        return rest;
+    }
+    return DISCARD;
+};
+
+export default dataStoreConflictHandler;
