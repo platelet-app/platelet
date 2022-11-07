dee15a6d358b7d0cae9577b9d705f0b1b80356d2
Theo Cranmore <theo.cranmore@gmail.com>
2022-11-07 21:29:28 +0000

    delete old tenantapi files

--- tenantapi/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "tenantconfigureapi": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
