13b9761e9d6aef907c4314c4c747c5a5f919a794
Theo Cranmore <theo.cranmore@gmail.com>
2022-11-16 19:49:12 +0000

    delete old login components

--- src/scenes/Login/components/LoginSkeleton.js
+++
@@ -1,9 +0,0 @@
-import Grid from "@mui/material/Grid";
-import Skeleton from '@mui/material/Skeleton';
-import React from "react";
-
-export default function LoginSkeleton(props) {
-    return (
-        <Skeleton variant="text" width={600} height={400}/>
-    )
-}
