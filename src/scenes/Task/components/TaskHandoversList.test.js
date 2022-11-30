9598541a57ac31af6fe5228c12ca1ac08988e8ee
Theo Cranmore <theo.cranmore@gmail.com>
2022-11-30 22:28:13 +0000

    some more task handover list tests

--- src/scenes/Task/components/TaskHandoversList.test.js
+++ src/scenes/Task/components/TaskHandoversList.test.js
@@ -84,15 +84,11 @@ describe("TaskHandoversList", () => {
         await waitFor(() => {
             expect(querySpy).toHaveBeenCalledWith(models.Handover);
         });
-        expect(screen.getByText(mockLocation.line1)).toBeInTheDocument();
-        expect(screen.getByText(mockLocation2.line1)).toBeInTheDocument();
-        expect(screen.getByText(mockLocation3.line1)).toBeInTheDocument();
-        expect(screen.getByText(mockLocation2.line1).nextSibling).toBe(
-            screen.getByText(mockLocation.line1)
-        );
-        //expect(screen.getByText(mockLocation.line1).nextSibling).toBe(
-        //    screen.getByText(mockLocation3.line1)
-        //);
+        const handoverList = await screen.findAllByRole("listitem");
+        expect(handoverList).toHaveLength(3);
+        expect(handoverList[0]).toHaveTextContent("Test Line 2");
+        expect(handoverList[1]).toHaveTextContent("Test Line 1");
+        expect(handoverList[2]).toHaveTextContent("Test Line 3");
     });
 
     test("add a new handover", async () => {
@@ -113,6 +109,7 @@ describe("TaskHandoversList", () => {
             expect(saveSpy).toHaveBeenCalledWith({
                 id: expect.any(String),
                 task: mockTask,
+                orderInGrid: 1,
                 tenantId,
             });
         });
@@ -136,6 +133,11 @@ describe("TaskHandoversList", () => {
                 tenantId,
             })
         );
+        const mockHandover = new models.Handover({
+            task: mockTask,
+            orderInGrid: 1,
+            tenantId,
+        });
         const querySpy = jest.spyOn(DataStore, "query");
         const saveSpy = jest.spyOn(DataStore, "save");
         render(<TaskHandoversList taskId={mockTask.id} />, { preloadedState });
@@ -145,9 +147,8 @@ describe("TaskHandoversList", () => {
         userEvent.click(screen.getByRole("button", { name: "Add handover" }));
         await waitFor(() => {
             expect(saveSpy).toHaveBeenCalledWith({
+                ...mockHandover,
                 id: expect.any(String),
-                task: mockTask,
-                tenantId,
             });
         });
         const locationBox = await screen.findByRole("combobox", {
@@ -157,10 +158,9 @@ describe("TaskHandoversList", () => {
         userEvent.click(await screen.findByText(mockLocation.name));
         await waitFor(() => {
             expect(saveSpy).toHaveBeenCalledWith({
+                ...mockHandover,
                 id: expect.any(String),
-                task: mockTask,
                 handoverLocation: mockLocation,
-                tenantId,
             });
         });
     });
@@ -212,7 +212,7 @@ describe("TaskHandoversList", () => {
         ).toBeInTheDocument();
     });
 
-    test("delete a handover", async () => {
+    test.skip("delete a handover", async () => {
         const mockTask = await DataStore.save(
             new models.Task({
                 status: models.TaskStatus.NEW,
@@ -238,6 +238,154 @@ describe("TaskHandoversList", () => {
         expect(
             screen.queryAllByRole("button", { name: "Clear handover" }).length
         ).toBe(0);
+    });
+
+    test("increment and decrement a handover in the list", async () => {
+        const mockTask = await DataStore.save(
+            new models.Task({
+                status: models.TaskStatus.NEW,
+                tenantId,
+            })
+        );
+        const mockLocation = await DataStore.save(
+            new models.Location({
+                name: "Test Location",
+                line1: "Test Line 1",
+                tenantId,
+            })
+        );
+        const mockLocation2 = await DataStore.save(
+            new models.Location({
+                name: "Test Location 2",
+                line1: "Test Line 2",
+                tenantId,
+            })
+        );
+        const mockLocation3 = await DataStore.save(
+            new models.Location({
+                name: "Test Location 3",
+                line1: "Test Line 3",
+                tenantId,
+            })
+        );
+        const mockHandover = await DataStore.save(
+            new models.Handover({
+                task: mockTask,
+                handoverLocation: mockLocation,
+                tenantId,
+                orderInGrid: 1,
+            })
+        );
+        const mockHandover2 = await DataStore.save(
+            new models.Handover({
+                task: mockTask,
+                handoverLocation: mockLocation2,
+                tenantId,
+                orderInGrid: 2,
+            })
+        );
+        const mockHandover3 = await DataStore.save(
+            new models.Handover({
+                task: mockTask,
+                handoverLocation: mockLocation3,
+                tenantId,
+                orderInGrid: 3,
+            })
+        );
+        const querySpy = jest.spyOn(DataStore, "query");
+        const saveSpy = jest.spyOn(DataStore, "save");
+        render(<TaskHandoversList taskId={mockTask.id} />, { preloadedState });
+        await waitFor(() => {
+            expect(querySpy).toHaveBeenCalled();
+        });
+        userEvent.click(
+            screen.getAllByRole("button", { name: "Move handover up" })[1]
+        );
+        await waitFor(() => {
+            expect(saveSpy).toHaveBeenCalledWith({
+                ...mockHandover2,
+                orderInGrid: 1,
+            });
+        });
+        await waitFor(() => {
+            expect(saveSpy).toHaveBeenCalledWith({
+                ...mockHandover,
+                orderInGrid: 2,
+            });
+        });
+        const handoverList = await screen.findAllByRole("listitem");
+        expect(handoverList[0]).toHaveTextContent(mockLocation2.line1);
+        expect(handoverList[1]).toHaveTextContent(mockLocation.line1);
+        expect(handoverList[2]).toHaveTextContent(mockLocation3.line1);
+        userEvent.click(
+            screen.getAllByRole("button", { name: "Move handover down" })[1]
+        );
+        await waitFor(() => {
+            expect(saveSpy).toHaveBeenCalledWith({
+                ...mockHandover,
+                orderInGrid: 3,
+            });
+        });
+        await waitFor(() => {
+            expect(saveSpy).toHaveBeenCalledWith({
+                ...mockHandover3,
+                orderInGrid: 2,
+            });
+        });
+        const handoverList2 = await screen.findAllByRole("listitem");
+        expect(handoverList2[0]).toHaveTextContent(mockLocation2.line1);
+        expect(handoverList2[1]).toHaveTextContent(mockLocation3.line1);
+        expect(handoverList2[2]).toHaveTextContent(mockLocation.line1);
+    });
+
+    test("disable the first and last up and down buttons", async () => {
+        const mockTask = await DataStore.save(
+            new models.Task({
+                status: models.TaskStatus.NEW,
+                tenantId,
+            })
+        );
+        const mockLocation = await DataStore.save(
+            new models.Location({
+                name: "Test Location",
+                line1: "Test Line 1",
+                tenantId,
+            })
+        );
+        const mockLocation2 = await DataStore.save(
+            new models.Location({
+                name: "Test Location 2",
+                line1: "Test Line 2",
+                tenantId,
+            })
+        );
+        await DataStore.save(
+            new models.Handover({
+                task: mockTask,
+                handoverLocation: mockLocation,
+                tenantId,
+                orderInGrid: 1,
+            })
+        );
+        await DataStore.save(
+            new models.Handover({
+                task: mockTask,
+                handoverLocation: mockLocation2,
+                tenantId,
+                orderInGrid: 2,
+            })
+        );
+        const querySpy = jest.spyOn(DataStore, "query");
+        render(<TaskHandoversList taskId={mockTask.id} />, { preloadedState });
+        await waitFor(() => {
+            expect(querySpy).toHaveBeenCalled();
+        });
+        expect(
+            screen.getAllByRole("button", { name: "Move handover up" })[0]
+        ).toBeDisabled();
+        expect(
+            screen.getAllByRole("button", { name: "Move handover down" })[1]
+        ).toBeDisabled();
     });
 
     test("the observer reacts to new handovers", async () => {
