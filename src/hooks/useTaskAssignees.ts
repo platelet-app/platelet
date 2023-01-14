71e699aac95a3d4c857e67fbbb73e49ea0546e85
Theo Cranmore <theo.cranmore@gmail.com>
2023-01-14 04:47:08 +0000

    useTaskAssignees hook

---
+++ src/hooks/useTaskAssignees.ts
@@ -0,0 +1,46 @@
+import { DataStore } from "aws-amplify";
+import * as models from "../models";
+import React from "react";
+import convertModelsToObject from "../utilities/convertModelsToObject";
+
+type TaskAssigneesType = {
+    [key: string]: models.TaskAssignee;
+};
+
+const useTaskAssignees = (taskId: string) => {
+    const [state, setState] = React.useState<TaskAssigneesType>({});
+    const [isFetching, setIsFetching] = React.useState(true);
+    const [error, setError] = React.useState<any>(null);
+
+    const observer = React.useRef({ unsubscribe: () => {} });
+
+    const getTaskAssignees = React.useCallback(async () => {
+        if (!taskId) return;
+        setIsFetching(true);
+        observer.current.unsubscribe();
+        try {
+            observer.current = DataStore.observeQuery(
+                models.TaskAssignee,
+                (c) => c.task.id.eq(taskId)
+            ).subscribe(({ items }) => {
+                const result = convertModelsToObject(items);
+                setIsFetching(false);
+                setState(result);
+            });
+        } catch (error) {
+            setError(error);
+            setIsFetching(false);
+        }
+    }, [taskId]);
+
+    React.useEffect(() => {
+        getTaskAssignees();
+        return () => {
+            observer.current.unsubscribe();
+        };
+    }, [getTaskAssignees]);
+
+    return { state, isFetching, error };
+};
+
+export default useTaskAssignees;
