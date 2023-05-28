2d104ed0f1aebd8566047f80c949d01848c75937
Theo Cranmore <theo.cranmore@gmail.com>
2023-05-28 23:03:32 +0100

    update plateletArchiver function to use appsync operations

--- amplify/backend/function/plateletArchiver/src/index.js
+++ amplify/backend/function/plateletArchiver/src/index.js
@@ -9,13 +9,16 @@ Amplify Params - DO NOT EDIT */
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 
-const { Sha256 } = require("@aws-crypto/sha256-js");
-const { defaultProvider } = require("@aws-sdk/credential-provider-node");
-const { SignatureV4 } = require("@aws-sdk/signature-v4");
-const { HttpRequest } = require("@aws-sdk/protocol-http");
-const { default: fetch, Request } = require("node-fetch");
 const moment = require("moment");
 const _ = require("lodash");
+const {
+    updateTask,
+    updateDeliverable,
+    updateLocation,
+    updateComment,
+    updateTaskAssignee,
+} = require("/opt/graphql/mutations");
+const { request, errorCheck } = require("/opt/appSyncRequest");
 
 const GRAPHQL_ENDPOINT = process.env.API_PLATELET_GRAPHQLAPIENDPOINTOUTPUT;
 
@@ -103,88 +106,6 @@ const getTaskCommentsQuery = /* GraphQL */ `
     };
 `;
 
-const updateTaskMutation = /* GraphQL */ `
-    mutation UpdateTask($input: UpdateTaskInput!) {
-        updateTask(input: $input) {
-            id
-            archived
-        }
-    }
-`;
-
-const updateDeliverableMutation = /* GraphQL */ `
-    mutation UpdateDeliverable($input: UpdateDeliverableInput!) {
-        updateDeliverable(input: $input) {
-            id
-            archived
-        };
-    };
-`;
-
-const updateCommentMutation = /* GraphQL */ `
-    mutation UpdateComment($input: UpdateCommentInput!) {
-        updateComment(input: $input) {
-            id
-            archived
-        };
-    };
-`;
-
-const updateLocationMutation = /* GraphQL */ `
-    mutation UpdateLocation($input: UpdateLocationInput!) {
-        updateLocation(input: $input) {
-            id
-            archived
-        };
-    };
-`;
-
-const updateTaskAssigneeMutation = /* GraphQL */ `
-    mutation UpdateTaskAssignee($input: UpdateTaskAssigneeInput!) {
-        updateTaskAssignee(input: $input) {
-            id
-            archived
-        };
-    };
-`;
-
-let makeNewRequest = async (query, variables) => {
-    const endpoint = new URL(GRAPHQL_ENDPOINT);
-    const signer = new SignatureV4({
-        credentials: defaultProvider(),
-        region: process.env.REGION,
-        service: "appsync",
-        sha256: Sha256,
-    });
-
-    const requestToBeSigned = new HttpRequest({
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            host: endpoint.host,
-        },
-        hostname: endpoint.host,
-        body: JSON.stringify({
-            query,
-            variables,
-        }),
-        path: endpoint.pathname,
-    });
-
-    const signed = await signer.sign(requestToBeSigned);
-    return new Request(endpoint, signed);
-};
-
-exports.makeNewRequest = makeNewRequest;
-
-const errorCheck = (body) => {
-    console.log("BODY", body);
-    if (body?.data?.errors) {
-        console.error(body?.data?.errors);
-        throw new Error(body?.data?.errors[0].message);
-    }
-};
-
 const getTaskAssignees = async (task) => {
     const items = [];
     let nextToken = null;
@@ -193,11 +114,13 @@ const getTaskAssignees = async (task) => {
             id: task.id,
             nextToken,
         };
-        const request = await makeNewRequest(getTaskAssigneesQuery, variables);
-        const response = await fetch(request);
+        const response = await request(
+            { query: getTaskAssigneesQuery, variables },
+            GRAPHQL_ENDPOINT
+        );
         const body = await response.json();
         errorCheck(body);
-        if (body.data.getTask) {
+        if (body?.data?.getTask?.assignees) {
             items.push(...body.data.getTask.assignees.items);
             nextToken = body.data.getTask.assignees.nextToken;
         } else {
@@ -215,14 +138,13 @@ const getDeliverables = async (task) => {
             id: task.id,
             nextToken,
         };
-        const request = await makeNewRequest(
-            getTaskDeliverablesQuery,
-            variables
+        const response = await request(
+            { query: getTaskDeliverablesQuery, variables },
+            GRAPHQL_ENDPOINT
         );
-        const response = await fetch(request);
         const body = await response.json();
         errorCheck(body);
-        if (body.data.getTask) {
+        if (body?.data?.getTask?.deliverables) {
             items.push(...body.data.getTask.deliverables.items);
             nextToken = body.data.getTask.deliverables.nextToken;
         } else {
@@ -240,11 +162,13 @@ const getComments = async (task) => {
             id: task.id,
             nextToken,
         };
-        const request = await makeNewRequest(getTaskCommentsQuery, variables);
-        const response = await fetch(request);
+        const response = await request(
+            { query: getTaskCommentsQuery, variables },
+            GRAPHQL_ENDPOINT
+        );
         const body = await response.json();
         errorCheck(body);
-        if (body.data.getTask) {
+        if (body?.data?.getTask?.comments) {
             items.push(...body.data.getTask.comments.items);
             nextToken = body.data.getTask.comments.nextToken;
         } else {
@@ -264,11 +188,10 @@ const getUnArchivedTasksByStatus = async (status) => {
             status: { eq: status },
             nextToken,
         };
-        const request = await makeNewRequest(query, variables);
-        const response = await fetch(request);
+        const response = await request({ query, variables }, GRAPHQL_ENDPOINT);
         const body = await response.json();
         errorCheck(body);
-        if (body.data.tasksByArchivedStatus) {
+        if (body?.data?.tasksByArchivedStatus) {
             items.push(...body.data.tasksByArchivedStatus.items);
             nextToken = body.data.tasksByArchivedStatus.nextToken;
         } else {
@@ -279,7 +202,7 @@ const getUnArchivedTasksByStatus = async (status) => {
     return items.flat();
 };
 
-const updateTaskAssignees = async (assignment) => {
+const updateTaskAssigneeItem = async (assignment) => {
     const variables = {
         input: {
             id: assignment.id,
@@ -287,14 +210,16 @@ const updateTaskAssignees = async (assignment) => {
             _version: assignment._version,
         },
     };
-    const request = await makeNewRequest(updateTaskAssigneeMutation, variables);
-    const response = await fetch(request);
+    const response = await request(
+        { query: updateTaskAssignee, variables },
+        GRAPHQL_ENDPOINT
+    );
     const body = await response.json();
     errorCheck(body);
     return body.data.updateTaskAssignee;
 };
 
-const updateDeliverable = async (deliverable) => {
+const updateDeliverableItem = async (deliverable) => {
     const variables = {
         input: {
             id: deliverable.id,
@@ -302,14 +227,16 @@ const updateDeliverable = async (deliverable) => {
             _version: deliverable._version,
         },
     };
-    const request = await makeNewRequest(updateDeliverableMutation, variables);
-    const response = await fetch(request);
+    const response = await request(
+        { query: updateDeliverable, variables },
+        GRAPHQL_ENDPOINT
+    );
     const body = await response.json();
     errorCheck(body);
     return body.data.updateDeliverable;
 };
 
-const updateComment = async (comment) => {
+const updateCommentItem = async (comment) => {
     const variables = {
         input: {
             id: comment.id,
@@ -317,14 +244,16 @@ const updateComment = async (comment) => {
             _version: comment._version,
         },
     };
-    const request = await makeNewRequest(updateCommentMutation, variables);
-    const response = await fetch(request);
+    const response = await request(
+        { query: updateComment, variables },
+        GRAPHQL_ENDPOINT
+    );
     const body = await response.json();
     errorCheck(body);
     return body.data.updateComment;
 };
 
-const updateLocation = async (location) => {
+const updateLocationItem = async (location) => {
     const variables = {
         input: {
             id: location.id,
@@ -332,14 +261,16 @@ const updateLocation = async (location) => {
             _version: location._version,
         },
     };
-    const request = await makeNewRequest(updateLocationMutation, variables);
-    const response = await fetch(request);
+    const response = await request(
+        { query: updateLocation, variables },
+        GRAPHQL_ENDPOINT
+    );
     const body = await response.json();
     errorCheck(body);
     return body.data.updateLocation;
 };
 
-const updateTask = async (task) => {
+const updateTaskItem = async (task) => {
     const variables = {
         input: {
             id: task.id,
@@ -347,18 +278,16 @@ const updateTask = async (task) => {
             _version: task._version,
         },
     };
-    const request = await makeNewRequest(updateTaskMutation, variables);
-    const response = await fetch(request);
+    const response = await request(
+        { query: updateTask, variables },
+        GRAPHQL_ENDPOINT
+    );
     const body = await response.json();
     errorCheck(body);
     return body.data.updateTask;
 };
 
-exports.handler = async (event, makeNewRequestTest) => {
-    if (makeNewRequestTest && process.env.NODE_ENV === "test") {
-        console.log("TESTING");
-        makeNewRequest = makeNewRequestTest;
-    }
+exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
     const tasks = await Promise.all(
         ["COMPLETED", "REJECTED", "ABANDONED", "CANCELLED"].map((status) =>
@@ -391,21 +320,21 @@ exports.handler = async (event, makeNewRequestTest) => {
                         establishmentLocation &&
                         establishmentLocation.listed === 0
                     ) {
-                        updateLocation(establishmentLocation);
+                        updateLocationItem(establishmentLocation);
                         console.log(
                             "Archived establishment location",
                             establishmentLocation.id
                         );
                     }
                     if (pickUpLocation && pickUpLocation.listed === 0) {
-                        updateLocation(pickUpLocation);
+                        updateLocationItem(pickUpLocation);
                         console.log(
                             "Archived pick up location",
                             pickUpLocation.id
                         );
                     }
                     if (dropOffLocation && dropOffLocation.listed === 0) {
-                        updateLocation(dropOffLocation);
+                        updateLocationItem(dropOffLocation);
                         console.log(
                             "Archived drop off location",
                             dropOffLocation.id
@@ -416,7 +345,7 @@ exports.handler = async (event, makeNewRequestTest) => {
                     console.log("Found assignees: ", assignees);
                     const updateAssigneesResult = await Promise.all(
                         assignees.map((assignment) =>
-                            updateTaskAssignees(assignment)
+                            updateTaskAssigneeItem(assignment)
                         )
                     );
                     console.log("updateAssigneesResult", updateAssigneesResult);
@@ -428,7 +357,7 @@ exports.handler = async (event, makeNewRequestTest) => {
                     console.log("Found deliverables: ", deliverables);
                     const updateDeliverablesResult = await Promise.all(
                         deliverables.map((deliverable) =>
-                            updateDeliverable(deliverable)
+                            updateDeliverableItem(deliverable)
                         )
                     );
                     console.log(
@@ -443,13 +372,13 @@ exports.handler = async (event, makeNewRequestTest) => {
                     const comments = await getComments(task);
                     console.log("Found comments: ", comments);
                     const updateCommentsResult = await Promise.all(
-                        comments.map((comment) => updateComment(comment))
+                        comments.map((comment) => updateCommentItem(comment))
                     );
                     console.log("updateCommentsResult", updateCommentsResult);
                     if (updateCommentsResult.some((a) => a.archived !== 1)) {
                         throw new Error("Failed to archive task comments");
                     }
-                    const updateTaskResult = await updateTask(task);
+                    const updateTaskResult = await updateTaskItem(task);
                     console.log("updateTaskResult", updateTaskResult);
                     if (updateTaskResult.archived !== 1) {
                         throw new Error("Failed to archive task");
