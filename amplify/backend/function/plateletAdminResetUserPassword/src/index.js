57b3f832cad3243b3b06fbb2b9178e5365a899b7
Theo Cranmore <theo.cranmore@gmail.com>
2023-06-03 22:06:48 +0100

    plateletAdminResetUserPassword lambda function

---
+++ amplify/backend/function/plateletAdminResetUserPassword/src/index.js
@@ -0,0 +1,21 @@
+/* Amplify Params - DO NOT EDIT
+	AUTH_PLATELET61A0AC07_USERPOOLID
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+};
