51729bfe4212f23399b1ec2234250046a4c352a1
Theo Cranmore <theo.cranmore@gmail.com>
2023-06-04 17:58:18 +0100

    plateletAdminAddNewUser now uses layer for sending email

--- amplify/backend/function/plateletAdminAddNewUser/src/index.js
+++ amplify/backend/function/plateletAdminAddNewUser/src/index.js
@@ -12,64 +12,9 @@ const uuid = require("uuid");
 const { createUser, deleteUser } = require("/opt/graphql/mutations");
 const { listUsers } = require("/opt/graphql/queries");
 const { request, errorCheck } = require("/opt/appSyncRequest");
+const { sendWelcomeEmail } = require("/opt/sendWelcomeEmail");
 
 const GRAPHQL_ENDPOINT = process.env.API_PLATELET_GRAPHQLAPIENDPOINTOUTPUT;
-
-async function sendWelcomeEmail(emailAddress, recipientName, password) {
-    const ses = new aws.SES({
-        apiVersion: "2010-12-01",
-        region: process.env.REGION,
-    });
-    const params = {
-        Destination: {
-            ToAddresses: [emailAddress],
-        },
-        Message: {
-            Body: {
-                Html: {
-                    Charset: "UTF-8",
-                    Data: `
-                    <p>
-                        Welcome to https://${process.env.PLATELET_DOMAIN_NAME}, ${recipientName}!
-                    </p>
-                    <p>
-                        An admin has created your account for you with a temporary password.
-                    </p>
-                    <p>
-                        <b>Username:</b> ${emailAddress}
-                    </p>
-                    <p>
-                        <b>Password:</b> ${password}
-                    </p>
-                    <p>
-                        <b>This temporary password will expire in one week.</b>
-                    </p>
-                    <p>
-                        Thank you.
-                    </p>
-                    `,
-                },
-                Text: {
-                    Charset: "UTF-8",
-                    Data: `Welcome to https://${process.env.PLATELET_DOMAIN_NAME}, ${recipientName}!
-                    An admin has created your account for you. A temporary password has been generated for you.
-                    Username: ${emailAddress}
-                    Password: ${password}
-                    Thank you.`,
-                },
-            },
-            Subject: {
-                Charset: "UTF-8",
-                Data: "Welcome to Platelet!",
-            },
-        },
-        Source: process.env.PLATELET_WELCOME_EMAIL,
-        ReplyToAddresses: [process.env.PLATELET_WELCOME_EMAIL],
-        ReturnPath: process.env.PLATELET_WELCOME_EMAIL,
-    };
-
-    return await ses.sendEmail(params).promise();
-}
 
 const getAllUsers = async (tenantId) => {
     const items = [];
