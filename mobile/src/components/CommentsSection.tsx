2a146c93dfbd7f7319d2d33a7a9fb15b2c256b4c
Theo Cranmore <theo.cranmore@gmail.com>
2023-08-01 01:33:35 +0100

    react native: sort comments by createdAt time

--- mobile/src/components/CommentsSection.tsx
+++ mobile/src/components/CommentsSection.tsx
@@ -3,6 +3,7 @@ import useComments from "../hooks/useComments";
 import GenericError from "../screens/Errors/GenericError";
 import CommentItem from "./CommentItem";
 import NewCommentCard from "./NewCommentCard";
+import * as models from "../models";
 
 type CommentsSectionProps = {
     parentId: string;
@@ -16,19 +17,25 @@ const CommentsSection: React.FC<CommentsSectionProps> = ({ parentId }) => {
     } else {
         return (
             <View style={{ gap: 4 }}>
-                {state.map((comment, index, array) => {
-                    const prevAuthorId =
-                        index > 0 && array[index - 1].author
-                            ? array[index - 1].author?.id
-                            : null;
-                    return (
-                        <CommentItem
-                            showAuthor={prevAuthorId !== comment.author?.id}
-                            key={comment.id}
-                            comment={comment}
-                        />
-                    );
-                })}
+                {state
+                    .sort(
+                        (a, b) =>
+                            +new Date(a.createdAt || new Date()) -
+                            +new Date(b.createdAt || new Date())
+                    )
+                    .map((comment, index, array) => {
+                        const prevAuthorId =
+                            index > 0 && array[index - 1].author
+                                ? array[index - 1].author?.id
+                                : null;
+                        return (
+                            <CommentItem
+                                showAuthor={prevAuthorId !== comment.author?.id}
+                                key={comment.id}
+                                comment={comment}
+                            />
+                        );
+                    })}
                 <NewCommentCard parentId={parentId} />
             </View>
         );
