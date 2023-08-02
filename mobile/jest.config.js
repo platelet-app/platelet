4907f944b07c7b995c74af106a796075c3db676c
Theo Cranmore <theo.cranmore@gmail.com>
2023-08-02 16:00:32 +0100

    react native: use jest-expo and test android and ios

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,17 +1,36 @@
 module.exports = {
-    setupFilesAfterEnv: [
-        "<rootDir>/setupTests.js",
-        "./node_modules/react-native-gesture-handler/jestSetup.js",
+    projects: [
+        {
+            preset: "jest-expo/ios",
+            setupFilesAfterEnv: [
+                "<rootDir>/setupTests.js",
+                "./node_modules/react-native-gesture-handler/jestSetup.js",
+            ],
+            setupFiles: [
+                "./node_modules/react-native-gesture-handler/jestSetup.js",
+            ],
+            transformIgnorePatterns: [
+                "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+                "node_modules/(?!(@aws-amplify/ui-react|@aws-amplify/DataStore))/",
+            ],
+            moduleNameMapper: { "^uuid$": "uuid" },
+            globalSetup: "./global-setup.js",
+        },
+        {
+            preset: "jest-expo/android",
+            setupFilesAfterEnv: [
+                "<rootDir>/setupTests.js",
+                "./node_modules/react-native-gesture-handler/jestSetup.js",
+            ],
+            setupFiles: [
+                "./node_modules/react-native-gesture-handler/jestSetup.js",
+            ],
+            transformIgnorePatterns: [
+                "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+                "node_modules/(?!(@aws-amplify/ui-react|@aws-amplify/DataStore))/",
+            ],
+            moduleNameMapper: { "^uuid$": "uuid" },
+            globalSetup: "./global-setup.js",
+        },
     ],
-    setupFiles: ["./node_modules/react-native-gesture-handler/jestSetup.js"],
-    preset: "jest-expo",
-    transformIgnorePatterns: [
-        "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
-        "node_modules/(?!(@aws-amplify/ui-react|@aws-amplify/DataStore))/",
-    ],
-    moduleNameMapper: {
-        // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
-        uuid: require.resolve("uuid"),
-    },
-    globalSetup: "./global-setup.js",
 };
--- mobile/setupTests.js
+++ mobile/setupTests.js
@@ -1,5 +1,6 @@
 import MockAsyncStorage from "mock-async-storage";
 import "@testing-library/jest-native/extend-expect";
+import mockSafeAreaContext from "react-native-safe-area-context/jest/mock";
 
 const mockImpl = new MockAsyncStorage();
 jest.mock("@react-native-async-storage/async-storage", () => mockImpl);
@@ -9,3 +10,4 @@ global.__reanimatedWorkletInit = () => {};
 jest.mock("react-native-reanimated", () =>
     require("react-native-reanimated/mock")
 );
+jest.mock("react-native-safe-area-context", () => mockSafeAreaContext);
