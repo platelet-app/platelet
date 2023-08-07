57a065617e57a7c1943bab012b62f009f4a40585
Theo Cranmore <theo.cranmore@gmail.com>
2023-08-07 18:15:21 +0100

    react native: fix address not showing when it is null

--- mobile/src/hooks/useModelSubscription.ts
+++ mobile/src/hooks/useModelSubscription.ts
@@ -25,6 +25,7 @@ const useModelSubscription = <T extends PersistentModel>(
     const getData = React.useCallback(async () => {
         if (!id) {
             setState(null);
+            setIsFetching(false);
             return;
         }
         observer.current.unsubscribe();
