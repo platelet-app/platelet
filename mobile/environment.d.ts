5052d8bf5fae97cc6eb57477a8dea02b444fe71d
Theo Cranmore <theo.cranmore@gmail.com>
2023-08-12 16:14:23 +0100

    react native: reduce environment.d.ts

--- mobile/environment.d.ts
+++ mobile/environment.d.ts
@@ -2,13 +2,7 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             NODE_ENV: "development" | "production";
-            REACT_APP_OFFLINE_ONLY: "true" | "false";
-            REACT_APP_POPULATE_FAKE_DATA: "true" | "false";
-            REACT_APP_DEMO_MODE: "true" | "false";
-            REACT_APP_DEMO_PROFILE_PICTURES_BUCKET_NAME?: string;
-            REACT_APP_DEMO_PROFILE_PICTURES_BUCKET_REGION?: string;
-            REACT_APP_GOOGLE_MAPS_API_KEY: string;
-            REACT_APP_TENANT_GRAPHQL_ENDPOINT: URL | "undefined";
+            REACT_APP_TENANT_GRAPHQL_ENDPOINT: URL | "undefined" | undefined;
             REACT_APP_TENANT_GRAPHQL_API_KEY: string;
         }
     }
