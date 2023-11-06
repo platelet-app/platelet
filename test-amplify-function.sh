9396392db6dabea2f8973391933b18c6a8ccd1be
Theo Cranmore <theo.cranmore@gmail.com>
2023-11-06 21:48:39 +0000

    amplify build step and bash script for testing lambdas

---
+++ test-amplify-function.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+cd amplify/backend/function
+
+for dir in */ ; do
+  cd "$dir"
+  echo "Current working directory: $PWD"
+    if [ -d 'src' ];
+    then
+        cd 'src'
+        yarn run test -- --watchAll=false
+        cd ..
+    fi
+    if [ -d 'opt' ];
+    then
+        yarn
+        yarn run test -- --watchAll=false
+    fi
+    cd ..
+done
