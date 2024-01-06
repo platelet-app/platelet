21a5ce01e0bd3c81e70f78aa99e6609c6862c0e7
Theo Cranmore <theo.cranmore@gmail.com>
2024-01-06 19:39:07 +0000

    saveAmplifyConfig throw error if REACT_APP_TENANT_GRAPHQL_ENDPOINT is not defined

--- src/utilities/saveAmplifyConfig.ts
+++ src/utilities/saveAmplifyConfig.ts
@@ -15,14 +15,16 @@ type TenantQueryVariables = {
     id: string;
 };
 
+const APPSYNC_API_URL = process.env.REACT_APP_TENANT_GRAPHQL_ENDPOINT;
+
 const fetchData = async (
     query: string,
     variables: TenantQueryVariables | null = null,
     timeout: number = 300000
 ) => {
+    if (!APPSYNC_API_URL) throw new Error("Tenant API URL is not defined");
     const controller = new AbortController();
     const id = setTimeout(() => controller.abort(), timeout);
-    const APPSYNC_API_URL = process.env.REACT_APP_TENANT_GRAPHQL_ENDPOINT;
     const credentialsAppSync = {
         "x-api-key": process.env.REACT_APP_TENANT_GRAPHQL_API_KEY,
     };
