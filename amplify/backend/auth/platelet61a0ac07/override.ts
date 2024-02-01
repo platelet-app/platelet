0f4412ac2c138209c9aa974d48b6a210b73aa7d2
Theo Cranmore <theo.cranmore@gmail.com>
2024-02-01 22:27:22 +0000

    cognito client overrides

---
+++ amplify/backend/auth/platelet61a0ac07/override.ts
@@ -0,0 +1,24 @@
+import {
+    AmplifyAuthCognitoStackTemplate,
+    AmplifyProjectInfo,
+} from "@aws-amplify/cli-extensibility-helper";
+
+export function override(
+    resources: AmplifyAuthCognitoStackTemplate,
+    amplifyProjectInfo: AmplifyProjectInfo
+) {
+    resources.userPoolClient.accessTokenValidity = 1;
+    resources.userPoolClient.idTokenValidity = 1;
+    resources.userPoolClientWeb.accessTokenValidity = 1;
+    resources.userPoolClientWeb.idTokenValidity = 1;
+    resources.userPoolClient.tokenValidityUnits = {
+        ...resources.userPoolClient.tokenValidityUnits,
+        accessToken: "days",
+        idToken: "days",
+    };
+    resources.userPoolClientWeb.tokenValidityUnits = {
+        ...resources.userPoolClient.tokenValidityUnits,
+        accessToken: "days",
+        idToken: "days",
+    };
+}
