1b1ad5a9875299626e36a9c38a95701eb946d280
Theo Cranmore <theo.cranmore@gmail.com>
2024-09-10 18:33:40 +0100

    fix?

--- src/views/Testimonials/Testimonials.js
+++ src/views/Testimonials/Testimonials.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Grid, Typography, Box, Avatar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import steve from "../../assets/images/steve.jpeg";
+import steve from "../../assets/images/steve.jpg";
 import joepic from "../../assets/images/joe.jpg";
 
 const useStyles = makeStyles((theme) => ({
