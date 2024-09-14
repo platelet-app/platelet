e3fecc9eb44ff74e1f7693a0406945b96ff12c06
Theo Cranmore <theo.cranmore@gmail.com>
2024-09-14 20:48:09 +0100

    video

--- src/views/IndexView/IndexView.js
+++ src/views/IndexView/IndexView.js
@@ -28,6 +28,24 @@ const IndexView = ({ themeMode }) => {
       <Section>
         <AboutPlatelet />
       </Section>
+      <SectionAlternate>
+        <div style={{ padding: "56.25% 0 0 0", position: "relative" }}>
+          <iframe
+            src="https://player.vimeo.com/video/1009494291?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+            frameborder="0"
+            allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
+            title="Platelet Dispatch Showcase"
+          ></iframe>
+        </div>
+        <script src="https://player.vimeo.com/api/player.js"></script>
+      </SectionAlternate>
     </div>
   );
 };
