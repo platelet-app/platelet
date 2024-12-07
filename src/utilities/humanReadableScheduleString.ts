a57affaf022a1bfc4a44c84b41cd34ebabff2ab8
Theo Cranmore <theo.cranmore@gmail.com>
2024-12-07 22:42:38 +0000

    feat/13: better schedule text

--- src/utilities/humanReadableScheduleString.ts
+++ src/utilities/humanReadableScheduleString.ts
@@ -2,15 +2,40 @@ import moment from "moment";
 import * as models from "../models";
 
 const humanReadableScheduleString = (schedule: models.Schedule) => {
+    let result = "";
     if (!schedule) return "";
     if (schedule.date) {
-        const date = new Date(schedule.date);
-        if (schedule.time) {
-            date.setHours(parseInt(schedule.time.split(":")[0]));
-            date.setMinutes(parseInt(schedule.time.split(":")[1]));
-        }
-        return moment(date).calendar();
+        const date = new Date(schedule.date).toISOString().split("T")[0];
+        result += moment(date).calendar(null, {
+            lastDay: "[Yesterday]",
+            sameDay: "[Today]",
+            nextDay: "[Tomorrow]",
+            lastWeek: "[last] dddd",
+            nextWeek: "dddd",
+            sameElse: "L",
+        });
     }
+    switch (schedule.relation) {
+        case models.TimeRelation.ANYTIME:
+            result += " at any time";
+            break;
+        case models.TimeRelation.BEFORE:
+            result += " before";
+            break;
+        case models.TimeRelation.AFTER:
+            result += " after";
+            break;
+        case models.TimeRelation.AT:
+            result += " at";
+            break;
+    }
+    if (schedule.time && schedule.relation !== models.TimeRelation.ANYTIME) {
+        const time = new Date();
+        time.setHours(parseInt(schedule.time.split(":")[0]));
+        time.setMinutes(parseInt(schedule.time.split(":")[1]));
+        result += ` ${moment(time).format("HH:mm")}`;
+    }
+    return result;
 };
 
 export default humanReadableScheduleString;
