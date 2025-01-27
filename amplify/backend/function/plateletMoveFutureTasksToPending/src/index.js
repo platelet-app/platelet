ae6945d554a886c5253fa634416ff6647c7c0732
Theo Cranmore <theo.cranmore@gmail.com>
2025-01-27 16:47:57 +0000

    feat/13: move future to pending initial lambda function

---
+++ amplify/backend/function/plateletMoveFutureTasksToPending/src/index.js
@@ -0,0 +1,22 @@
+/* Amplify Params - DO NOT EDIT
+	API_PLATELET_GRAPHQLAPIENDPOINTOUTPUT
+	API_PLATELET_GRAPHQLAPIIDOUTPUT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  },
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+};
