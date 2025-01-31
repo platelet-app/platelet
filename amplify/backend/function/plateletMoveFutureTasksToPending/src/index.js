19033659dd28cdea89614ba50b7a39073da27457
Theo Cranmore <theo.cranmore@gmail.com>
2025-01-31 17:49:10 +0000

    feat/13: plateletMoveFutureTasksToPending implementation

--- amplify/backend/function/plateletMoveFutureTasksToPending/src/index.js
+++ amplify/backend/function/plateletMoveFutureTasksToPending/src/index.js
@@ -8,15 +8,119 @@ Amplify Params - DO NOT EDIT */
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+const { request, errorCheck } = require("/opt/appSyncRequest");
+const { updateTask } = require("/opt/graphql/mutations");
+
+const GRAPHQL_ENDPOINT = process.env.API_PLATELET_GRAPHQLAPIENDPOINTOUTPUT;
+
+const query = /* GraphQL */ `
+    query LIST_TASKS_BY_STATUS($status: TaskStatus!, $nextToken: String) {
+        tasksByStatus(status: $status, nextToken: $nextToken) {
+            items {
+                id
+                status
+                pickUpSchedule {
+                    timePrimary
+                }
+                dropOffSchedule {
+                    timePrimary
+                }
+                _version
+            }
+            nextToken
+        }
+    }
+`;
+
+const getFutureTasks = async () => {
+    const items = [];
+    let nextToken = null;
+
+    do {
+        const variables = {
+            status: "FUTURE",
+            nextToken,
+        };
+        const response = await request({ query, variables }, GRAPHQL_ENDPOINT);
+        const body = await response.json();
+        errorCheck(body);
+        if (body?.data?.tasksByStatus) {
+            items.push(...body.data.tasksByStatus.items);
+            nextToken = body.data.tasksByStatus.nextToken;
+        } else {
+            nextToken = null;
+        }
+    } while (nextToken);
+
+    return items.flat();
+};
+
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+    const futureTasks = await getFutureTasks();
+    for (const task of futureTasks) {
+        if (task && task.pickUpSchedule) {
+            if (task.pickUpSchedule.timePrimary) {
+                const pickUpTime = new Date(task.pickUpSchedule.timePrimary);
+                const now = new Date();
+                now.setDate(now.getDate() + 1);
+                if (pickUpTime < now) {
+                    console.log(
+                        "Task is due to be picked up within 24 hours, moving to PENDING",
+                        task?.id
+                    );
+                    const variables = {
+                        input: {
+                            id: task.id,
+                            status: "PENDING",
+                            _version: task._version,
+                        },
+                    };
+                    const response = await request(
+                        {
+                            query: updateTask,
+                            variables,
+                        },
+                        GRAPHQL_ENDPOINT
+                    );
+                    const body = await response.json();
+                    errorCheck(body);
+                    console.log("Response:", body);
+                }
+            }
+        } else if (task && task.dropOffSchedule) {
+            if (task && task.dropOffSchedule) {
+                if (task.dropOffSchedule.timePrimary) {
+                    const dropOffTime = new Date(
+                        task.dropOffSchedule.timePrimary
+                    );
+                    const now = new Date();
+                    now.setDate(now.getDate() + 1);
+                    if (dropOffTime < now) {
+                        console.log(
+                            "Task is due to be dropped off within 24 hours, moving to PENDING",
+                            task?.id
+                        );
+                        const variables = {
+                            input: {
+                                id: task.id,
+                                status: "PENDING",
+                                _version: task._version,
+                            },
+                        };
+                        const response = await request(
+                            {
+                                query: updateTask,
+                                variables,
+                            },
+                            GRAPHQL_ENDPOINT
+                        );
+                        const body = await response.json();
+                        errorCheck(body);
+                        console.log("Response:", body);
+                    }
+                }
+            }
+        }
+    }
 };
