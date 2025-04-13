2a55841b4f9c53f336794b23de72871fda52e075
Theo Cranmore <theo.cranmore@gmail.com>
2025-04-13 08:14:52 +0100

    feat/13: taskScheduleDueStatus update

--- src/utilities/taskScheduleDueStatus.ts
+++ src/utilities/taskScheduleDueStatus.ts
@@ -5,21 +5,21 @@ const taskScheduleDueStatus = (
     hours: number = 0,
     days: number = 0
 ) => {
-    if (
-        [models.TimeRelation.ANYTIME, models.TimeRelation.AFTER].includes(
-            schedule?.relation as models.TimeRelation
-        )
-    ) {
-        return false;
-    }
     if (!schedule) {
         return false;
     }
     const now = new Date();
     const scheduleDate = new Date(schedule?.timePrimary ?? "");
+    if (
+        [models.TimeRelation.ANYTIME, models.TimeRelation.AFTER].includes(
+            schedule?.relation as models.TimeRelation
+        )
+    ) {
+        scheduleDate.setHours(23);
+        scheduleDate.setMinutes(59);
+    }
     scheduleDate.setUTCHours(scheduleDate.getUTCHours() - hours);
     scheduleDate.setUTCDate(scheduleDate.getUTCDate() - days);
-
     if (scheduleDate < now) {
         return true;
     }
