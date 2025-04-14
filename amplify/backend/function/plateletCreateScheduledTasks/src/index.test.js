50c720bef4cc03d4c8dd202ab97c58e26e1e3ee9
Theo Cranmore <theo.cranmore@gmail.com>
2025-04-14 13:57:50 +0100

    feat/13: plateletCreateScheduledTasks create the schedule data properly

--- amplify/backend/function/plateletCreateScheduledTasks/src/index.test.js
+++ amplify/backend/function/plateletCreateScheduledTasks/src/index.test.js
@@ -9,16 +9,8 @@ const mockScheduledTasks = [
             name: "name",
             telephoneNumber: "phone",
         },
-        pickUpSchedule: {
-            timePrimary: "2021-05-01T23:00:00.000Z",
-            timeSecondary: "2021-05-01T23:00:00.000Z",
-            relation: "BETWEEN",
-        },
-        dropOffSchedule: {
-            timePrimary: "2021-05-01T23:00:00.000Z",
-            timeSecondary: null,
-            relation: "BEFORE",
-        },
+        pickUpSchedule: null,
+        dropOffSchedule: null,
         establishmentLocation: {
             id: "establishmentLocationId",
             listed: 1,
@@ -90,6 +82,81 @@ const mockScheduledTasks = [
     },
 ];
 
+const mockScheduledTasksScheduled = [
+    {
+        id: "taskId",
+        tenantId: "tenantId",
+        priority: "MEDIUM",
+        requesterContact: {
+            name: "name",
+            telephoneNumber: "phone",
+        },
+        pickUpSchedule: {
+            timePrimary: "2099-01-01T10:00:00.000Z",
+            timeSecondary: "2099-01-02T04:00:00.000Z",
+            relation: "BETWEEN",
+        },
+        dropOffSchedule: {
+            timePrimary: "2099-01-01T23:00:00.000Z",
+            timeSecondary: null,
+            relation: "BEFORE",
+        },
+        establishmentLocation: {
+            id: "establishmentLocationId",
+            listed: 1,
+            _version: 1,
+        },
+        pickUpLocation: {
+            id: "pickUpLocationId",
+            listed: 1,
+            _version: 1,
+        },
+        dropOffLocation: {
+            id: "dropOffLocationId",
+            contact: {
+                name: "name",
+                telephoneNumber: "phone",
+            },
+            line1: "line1",
+            line2: "line2",
+            line3: "line3",
+            town: "town",
+            county: "county",
+            country: "country",
+            postcode: "postcode",
+            what3words: "what3words",
+            tenantId: "tenantId",
+            listed: 0,
+            _version: 1,
+        },
+        deliverables: {
+            items: [
+                {
+                    _deleted: null,
+                    id: "deliverableId",
+                    deliverableType: { id: "deliverableType", label: "bloods" },
+                    deliverableTypeDeliverablesId:
+                        "deliverableTypeDeliverablesId",
+                    count: 1,
+                    unit: "unit",
+                },
+                {
+                    _deleted: true,
+                    id: "deletedDeliverableId",
+                    deliverableType: { id: "deliverableType", label: "bloods" },
+                    deliverableTypeDeliverablesId:
+                        "deliverableTypeDeliverablesId",
+                    count: 1,
+                    unit: "unit",
+                },
+            ],
+        },
+        _version: 1,
+        _deleted: null,
+        _lastChangedAt: 1620000000000,
+    },
+];
+
 jest.mock(
     "/opt/appSyncRequest",
     () => {
@@ -127,6 +194,37 @@ describe("plateletCreateScheduledTasks", () => {
                     data: {
                         listScheduledTasks: {
                             items: mockScheduledTasks,
+                            nextToken: null,
+                        },
+                    },
+                }),
+            })
+            .mockResolvedValueOnce({
+                json: () => ({
+                    data: { createLocation: { id: "newLocationId" } },
+                }),
+            })
+            .mockResolvedValueOnce({
+                json: () => ({
+                    data: { createTask: { id: "newTaskId" } },
+                }),
+            })
+            .mockResolvedValueOnce({
+                json: () => ({
+                    data: { createDeliverable: { id: "newDeliverableId" } },
+                }),
+            });
+
+        await indexModule.handler();
+        expect(appsyncModule.request).toMatchSnapshot();
+    });
+    test("create some scheduled tasks and calculate schedule", async () => {
+        appsyncModule.request
+            .mockResolvedValueOnce({
+                json: () => ({
+                    data: {
+                        listScheduledTasks: {
+                            items: mockScheduledTasksScheduled,
                             nextToken: null,
                         },
                     },
