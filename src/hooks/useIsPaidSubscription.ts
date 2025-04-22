401452f4dc6ba67203ff6e5931b444db4aa32abe
Theo Cranmore <theo.cranmore@gmail.com>
2025-04-22 09:48:47 +0100

    feat/222: demo mode always return true for is paid hook

--- src/hooks/useIsPaidSubscription.ts
+++ src/hooks/useIsPaidSubscription.ts
@@ -7,11 +7,14 @@ const useIsPaidSubscription = () => {
         const user = await Auth.currentSession();
         const accessToken = user.getAccessToken();
         const groups = accessToken.payload["cognito:groups"];
-        console.log("GROUPS", groups);
         if (groups) setIsPaid(groups.includes("PAID"));
         else setIsPaid(false);
     }, []);
     React.useEffect(() => {
+        if (process.env.REACT_APP_DEMO_MODE === "true") {
+            setIsPaid(true);
+            return;
+        }
         getData();
     }, [getData]);
     return isPaid;
