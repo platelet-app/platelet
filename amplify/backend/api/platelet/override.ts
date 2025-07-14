703be5555b81765334e14f6d7161ce5b175084f6
Theo Cranmore <theo.cranmore@gmail.com>
2025-07-14 13:14:26 +0100

    overrides

--- amplify/backend/api/platelet/override.ts
+++ amplify/backend/api/platelet/override.ts
@@ -1,17 +1,6 @@
 import { AmplifyApiGraphQlResourceStackTemplate } from "@aws-amplify/cli-extensibility-helper";
 import { overrideDataSourceByFileName } from "./overrideHelpers"; // <<== the helper file in this repo
 
-export const override = (resources: AmplifyApiGraphQlResourceStackTemplate) => {
-    overrideDataSourceByFileName(
-        resources,
-        "Mutation.createTaskAssignee.postAuth.2", // <== The name of your file (without the extension)
-        "TaskAssignee", // <== The model that this resolver falls within
-        "TaskTable" // <== The new datasource that you want to use
-    );
-    overrideDataSourceByFileName(
-        resources,
-        "Mutation.createComment.postAuth.2", // <== The name of your file (without the extension)
-        "Comment", // <== The model that this resolver falls within
-        "UserTable" // <== The new datasource that you want to use
-    );
-};
+export const override = (
+    resources: AmplifyApiGraphQlResourceStackTemplate
+) => {};
