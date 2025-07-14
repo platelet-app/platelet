363ef6a97f42a3dc306dc55aaf93bf6e316a7ede
Theo Cranmore <theo.cranmore@gmail.com>
2025-07-14 14:26:49 +0100

    Revert "overrides"
    This reverts commit 703be5555b81765334e14f6d7161ce5b175084f6.

--- amplify/backend/api/platelet/override.ts
+++ amplify/backend/api/platelet/override.ts
@@ -1,6 +1,17 @@
 import { AmplifyApiGraphQlResourceStackTemplate } from "@aws-amplify/cli-extensibility-helper";
 import { overrideDataSourceByFileName } from "./overrideHelpers"; // <<== the helper file in this repo
 
-export const override = (
-    resources: AmplifyApiGraphQlResourceStackTemplate
-) => {};
+export const override = (resources: AmplifyApiGraphQlResourceStackTemplate) => {
+    overrideDataSourceByFileName(
+        resources,
+        "Mutation.createTaskAssignee.postAuth.2", // <== The name of your file (without the extension)
+        "TaskAssignee", // <== The model that this resolver falls within
+        "TaskTable" // <== The new datasource that you want to use
+    );
+    overrideDataSourceByFileName(
+        resources,
+        "Mutation.createComment.postAuth.2", // <== The name of your file (without the extension)
+        "Comment", // <== The model that this resolver falls within
+        "UserTable" // <== The new datasource that you want to use
+    );
+};
