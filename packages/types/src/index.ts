c31a94eff930c7772be5486e91578b9c3d73a77f
Theo Cranmore <theo.cranmore@gmail.com>
2025-09-29 23:35:01 +0100

    feat/228: workflow updates

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,3 +1,5 @@
-export * from "./API";
-export * from "./models";
-export * from "./graphql";
+export * as API from "./API";
+export * as models from "./models";
+export * as mutations from "./graphql/mutations";
+export * as queries from "./graphql/queries";
+export * as subscriptions from "./graphql/subscriptions";
