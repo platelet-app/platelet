960b7b8279aefa2ecb3f4fb40d0b81c785409d9c
Theo Cranmore <theo.cranmore@gmail.com>
2025-10-01 02:25:13 +0100

    feat/228: stuff

--- packages/types/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * as API from "./API";
-export * as models from "./models";
-export * as mutations from "./graphql/mutations";
-export * as queries from "./graphql/queries";
-export * as subscriptions from "./graphql/subscriptions";
