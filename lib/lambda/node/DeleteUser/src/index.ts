d12e68d6288715c729be4eca6918ac8fc03e8d9c
Theo Cranmore <theo.cranmore@gmail.com>
2025-10-17 13:14:35 +0100

    delete user profile picture

--- lib/lambda/node/DeleteUser/src/index.ts
+++ lib/lambda/node/DeleteUser/src/index.ts
@@ -2,12 +2,13 @@ import type { LambdaEvent, LambdaReturn } from "./interfaces.js";
 import { request, errorCheck } from "@platelet-app/lambda";
 import { getUser } from "./queries.js";
 import { mutations } from "@platelet-app/graphql";
-import type { User } from "@platelet-app/types";
+import type { User, S3Object } from "@platelet-app/types";
 import {
   AdminDeleteUserCommand,
   AdminDisableUserCommand,
   CognitoIdentityProviderClient,
 } from "@aws-sdk/client-cognito-identity-provider";
+import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
 
 const disableUserCognito = async (username: string, userPoolId: string) => {
   const params = {
@@ -47,6 +48,17 @@ const deleteUserFunction = async (user: User, endpoint: string) => {
   errorCheck(body);
 };
 
+const deleteProfilePicture = async (item: S3Object) => {
+  const config = {};
+  const client = new S3Client(config);
+  const input = {
+    Bucket: item.bucket,
+    Key: item.key,
+  };
+  const command = new DeleteObjectCommand(input);
+  await client.send(command);
+};
+
 const getUserFunction = async (userId: string, endpoint: string) => {
   const variables = {
     id: userId,
@@ -67,5 +79,6 @@ export const handler = async (event: LambdaEvent): Promise<LambdaReturn> => {
   await disableUserCognito(user.username, userPoolId);
   await deleteUserFunction(user, graphQLEndpoint);
   await deleteUserCognito(user.username, userPoolId);
+  await deleteProfilePicture(user.profilePicture);
   return { graphQLEndpoint, userId, userPoolId, retryCount };
 };
