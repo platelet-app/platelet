17ccae0940b88416178f5f56b8889d77ddff4484
Theo Cranmore <theo.cranmore@gmail.com>
2025-10-21 18:13:11 +0100

    feat/233: cdk-nag fix errors

--- cdk/bin/platelet-supporting-cdk.ts
+++ cdk/bin/platelet-supporting-cdk.ts
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { StepFunctionsStack } from "../lib/step-functions-stack";
+import { AwsSolutionsChecks } from "cdk-nag";
 
 const app = new cdk.App();
 
 const deployEnv = process.env.DEPLOY_ENV || "dev";
 
 new StepFunctionsStack(app, `StepFunctionsStack-${deployEnv}`, {
-  env: { account: "130063560692", region: "eu-west-1" },
+    env: { account: "130063560692", region: "eu-west-1" },
 });
+
+cdk.Aspects.of(app).add(new AwsSolutionsChecks());
