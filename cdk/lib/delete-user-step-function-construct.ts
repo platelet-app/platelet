fe81f873280a7814649cd2b7d31ed29a59b5e19d
Theo Cranmore <theo.cranmore@gmail.com>
2025-10-23 17:47:43 +0100

    fix permissions

--- cdk/lib/delete-user-step-function-construct.ts
+++ cdk/lib/delete-user-step-function-construct.ts
@@ -235,7 +235,7 @@ export class DeleteUserStepFunction extends Construct {
             }
         );
         this.createLambdaStatement(deleteAssignmentsFunction, {
-            queries: ["deleteTaskAssignee"],
+            mutations: ["deleteTaskAssignee"],
         });
 
         const cleanVehicleAssignmentsFunction = new lambda.Function(
