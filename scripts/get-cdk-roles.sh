8b59575a276f319808c8e21c871e0ea25997aaaf
Theo Cranmore <theo.cranmore@gmail.com>
2025-12-06 00:48:03 +0000

    feat/237: get-cdk-roles script take out user params

--- scripts/get-cdk-roles.sh
+++ scripts/get-cdk-roles.sh
@@ -31,3 +31,23 @@ echo "
         ]
     }
 ]" > "./amplify/backend/function/plateletAdminDeleteUser/custom-policies.json"
+
+USER_TAKE_OUT_DATA_STATE_MACHINE_ARN=$(jq '.[] | to_entries[] | select(.key|contains("TakeOutUserDataStateMachineArnOutput")).value' $1)
+
+USER_TAKE_OUT_DATA_STATE_MACHINE_ARN_SSM_PARAM_ARN=$(jq '.[] | to_entries[] | select(.key|contains("TakeOutUserDataStateMachineArnSSMParamArnOutput")).value' $1)
+
+echo "
+[
+    {
+        \"Action\": [
+            \"states:StartExecution\",
+            \"ssm:GetParameter\",
+            \"ssm:GetParameters\",
+            \"ssm:GetParametersByPath\"
+        ],
+        \"Resource\": [
+            $DELETE_USER_STATE_MACHINE_ARN,
+            $DELETE_USER_STATE_MACHINE_ARN_SSM_PARAM_ARN
+        ]
+    }
+]" > "./amplify/backend/function/plateletUserTakeOutData/custom-policies.json"
