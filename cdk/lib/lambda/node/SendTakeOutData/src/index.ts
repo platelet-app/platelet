a764aba3f0f9bc465b5485a381eaf5cd32ef47d4
Theo Cranmore <theo.cranmore@gmail.com>
2025-12-14 19:02:29 +0000

    feat/237: send email lambda work

--- cdk/lib/lambda/node/SendTakeOutData/src/index.ts
+++ cdk/lib/lambda/node/SendTakeOutData/src/index.ts
@@ -1,18 +1,21 @@
 import type { LambdaEvent, LambdaReturn } from "./interfaces.js";
+import nodemailer from "nodemailer";
 import {
     request,
     errorCheck,
     getUserProfilePictures,
 } from "@platelet-app/lambda";
 import { getUser } from "./queries.js";
-import type { S3Object, User } from "@platelet-app/types";
+import type { User } from "@platelet-app/types";
 import {
     S3Client,
     DeleteObjectCommand,
     PutObjectCommand,
     type ListObjectsV2CommandOutput,
     CopyObjectCommand,
+    GetObjectCommand,
 } from "@aws-sdk/client-s3";
+import { SESv2Client, SendEmailCommand } from "@aws-sdk/client-sesv2";
 
 const TAKE_OUT_BUCKET = process.env.TAKE_OUT_BUCKET;
 const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT;
@@ -46,12 +49,12 @@ const writeProfilePictures = async (
     }
 };
 
-const deleteTakeOutFile = async (item: S3Object) => {
+const deleteTakeOutFile = async (key: string) => {
     const config = {};
     const client = new S3Client(config);
     const input = {
-        Bucket: item.bucket,
-        Key: item.key,
+        Bucket: TAKE_OUT_BUCKET,
+        Key: key,
     };
     const command = new DeleteObjectCommand(input);
     await client.send(command);
@@ -65,6 +68,61 @@ const getUserFunction = async (userId: string, endpoint: string) => {
     const body = await response.json();
     errorCheck(body);
     return body?.data?.getUser;
+};
+
+function getS3File(item: string) {
+    const s3Client = new S3Client({ region: REGION || "eu-west-1" });
+    const input = {
+        Bucket: TAKE_OUT_BUCKET,
+        Key: item,
+    };
+    const command = new GetObjectCommand(input);
+    return s3Client.send(command);
+}
+
+const sendEmail = async (
+    emailAddress: string,
+    recipientName: string,
+    attachmentKey: string
+) => {
+    const html = `
+<p>
+    Dear ${recipientName}
+</p>
+<p>
+    Please find attached your requested take out data.
+</p>
+<p>
+    Thank you.
+</p>
+`;
+
+    const file = await getS3File(attachmentKey);
+    const content = await file.Body?.transformToString();
+    var mailOptions = {
+        from: "noreply@platelet.app",
+        subject: "Your requested take out data",
+        html,
+        to: emailAddress,
+        // bcc: Any BCC address you want here in an array,
+        attachments: [
+            {
+                filename: "An Attachment.pdf",
+                content,
+            },
+        ],
+    };
+
+    console.log("Creating SES transporter");
+    // create Nodemailer SES transporter
+    //
+    const sesClient = new SESv2Client({ region: REGION || "eu-west-1" });
+    var transporter = nodemailer.createTransport({
+        SES: { sesClient, SendEmailCommand },
+    });
+
+    // send email
+    transporter.sendMail(mailOptions);
 };
 
 export const handler = async (event: LambdaEvent): Promise<LambdaReturn> => {
@@ -83,5 +141,7 @@ export const handler = async (event: LambdaEvent): Promise<LambdaReturn> => {
             userId
         );
     }
+    await sendEmail(user?.emailAddress, user?.name, user.id);
+    await deleteTakeOutFile(user.id);
     return { retryCount, userId };
 };
