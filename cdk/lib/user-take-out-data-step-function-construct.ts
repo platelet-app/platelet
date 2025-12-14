400ff8860558044532dd857d0444a4f6eba2a707
Theo Cranmore <theo.cranmore@gmail.com>
2025-12-14 20:27:01 +0000

    feat/237: fix perms

--- cdk/lib/user-take-out-data-step-function-construct.ts
+++ cdk/lib/user-take-out-data-step-function-construct.ts
@@ -224,8 +224,14 @@ export class UserTakeOutDataStepFunction extends Construct {
 
         finishAndSendUserDataFunction.addToRolePolicy(
             new iam.PolicyStatement({
-                actions: ["s3:GetObject", "s3:ListBucket"],
+                actions: ["s3:GetObject"],
                 resources: [`${this.bucket.bucketArn}/public/*`],
+            })
+        );
+        finishAndSendUserDataFunction.addToRolePolicy(
+            new iam.PolicyStatement({
+                actions: ["s3:ListBucket"],
+                resources: [this.bucket.bucketArn],
             })
         );
         finishAndSendUserDataFunction.addToRolePolicy(
