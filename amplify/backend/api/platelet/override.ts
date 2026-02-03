fc61a63a25e3492867c87ff81a2dd49efc6226b8
Theo Cranmore <theo.cranmore@gmail.com>
2026-02-03 23:21:40 +0000

    feat/247: prevent assigning user to task if they are being deleted

--- amplify/backend/api/platelet/override.ts
+++ amplify/backend/api/platelet/override.ts
@@ -2,16 +2,25 @@ import { AmplifyApiGraphQlResourceStackTemplate } from "@aws-amplify/cli-extensi
 import { overrideDataSourceByFileName } from "./overrideHelpers"; // <<== the helper file in this repo
 
 export const override = (resources: AmplifyApiGraphQlResourceStackTemplate) => {
+    // prevent an assignment being made on a task if it is archived
     overrideDataSourceByFileName(
         resources,
         "Mutation.createTaskAssignee.postAuth.2", // <== The name of your file (without the extension)
         "TaskAssignee", // <== The model that this resolver falls within
         "TaskTable" // <== The new datasource that you want to use
     );
+    // prevent a user from creating a comment for another user
     overrideDataSourceByFileName(
         resources,
         "Mutation.createComment.postAuth.2", // <== The name of your file (without the extension)
         "Comment", // <== The model that this resolver falls within
+        "UserTable" // <== The new datasource that you want to use
+    );
+    // prevent a user being assigned to a task if they are being deleted
+    overrideDataSourceByFileName(
+        resources,
+        "Mutation.createTaskAssignee.postAuth.1", // <== The name of your file (without the extension)
+        "TaskAssignee", // <== The model that this resolver falls within
         "UserTable" // <== The new datasource that you want to use
     );
 };
