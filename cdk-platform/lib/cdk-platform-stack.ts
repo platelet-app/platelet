69aa141f70f95400d263c83c93a6889bc0646556
Theo Cranmore <theo.cranmore@gmail.com>
2026-05-26 09:28:09 +0100

    feat/254: init cdk platform

---
+++ cdk-platform/lib/cdk-platform-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkPlatformStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkPlatformQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
