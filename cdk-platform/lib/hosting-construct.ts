613417922c33286b0a396979d88aa01b8822ee68
Theo Cranmore <theo.cranmore@gmail.com>
2026-06-11 09:19:48 +0100

    feat/252: cloudfront distribution

--- cdk-platform/lib/hosting-construct.ts
+++ cdk-platform/lib/hosting-construct.ts
@@ -1,6 +1,9 @@
 import { Construct } from "constructs";
+import { Distribution, OriginAccessIdentity } from "@aws-cdk/aws-cloudfront";
+import { S3Origin } from "@aws-cdk/aws-cloudfront-origins";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { RemovalPolicy } from "aws-cdk-lib";
 
 export interface HostingConstructProps {
     region: string;
@@ -11,10 +14,24 @@ export class HostingConstruct extends Construct {
         super(scope, id);
         const landingBucket = new s3.Bucket(this, "PlateletLandingPage", {
             accessControl: s3.BucketAccessControl.PRIVATE,
+            removalPolicy: RemovalPolicy.DESTROY,
         });
         new BucketDeployment(this, "BucketDeployment", {
             destinationBucket: landingBucket,
             sources: [Source.asset("./lib/hosting/landing-page/build")],
+        });
+
+        const originAccessIdentity = new OriginAccessIdentity(
+            this,
+            "LandingOriginAccessIdentity"
+        );
+        landingBucket.grantRead(originAccessIdentity);
+
+        new Distribution(this, "Platelet.AppDistribution", {
+            defaultRootObject: "index.html",
+            defaultBehavior: {
+                origin: new S3Origin(landingBucket, { originAccessIdentity }),
+            },
         });
     }
 }
