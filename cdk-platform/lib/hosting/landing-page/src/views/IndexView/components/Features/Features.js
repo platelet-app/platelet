b63b7ba85f130923eeb8b1832f438263da35e6ad
Theo Cranmore <theo.cranmore@gmail.com>
2026-06-11 08:57:19 +0100

    feat/252: fix linter warnings

--- cdk-platform/lib/hosting/landing-page/src/views/IndexView/components/Features/Features.js
+++ cdk-platform/lib/hosting/landing-page/src/views/IndexView/components/Features/Features.js
@@ -1,119 +1,118 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Grid, colors, makeStyles, useTheme } from "@material-ui/core";
+import { Grid, colors, makeStyles } from "@material-ui/core";
 import { SectionHeader, IconAlternate } from "components/molecules";
 import { CardBase, DescriptionListIcon } from "components/organisms";
 
 const useStyles = makeStyles(() => ({
-  fontWeight900: {
-    fontWeight: 900,
-  },
+    fontWeight900: {
+        fontWeight: 900
+    }
 }));
 
 const data = [
-  {
-    icon: "fas fa-file-invoice",
-    color: colors.common.white,
-    title: "Reporting",
-    subtitle:
-      "Statistics and reporting features, with the ability to export your data.",
-  },
-  {
-    icon: "fas fa-motorcycle",
-    color: colors.common.white,
-    title: "Fleet management",
-    subtitle: "Add your vehicles and assign riders to them.",
-  },
-  {
-    icon: "fas fa-mobile-alt",
-    color: colors.common.white,
-    title: "Use anywhere",
-    subtitle:
-      "Developed with standard web technology for compatibility across a broad range of devices.",
-  },
-  {
-    icon: "fa-solid fa-hand-pointer",
-    color: colors.common.white,
-    title: "Easy to use",
-    subtitle:
-      "Used by volunteers across the country, Platelet Dispatch lets them save lives without slowing them down.",
-  },
-  {
-    icon: "fas fa-rocket",
-    color: colors.common.white,
-    title: "Update and track consignment status",
-    subtitle:
-      "Mark consignments as Active, Picked Up, Delivered, Rider Home, or Cancelled, Rejected and Abandoned.",
-  },
-  {
-    icon: "fa-solid fa-clock-rotate-left",
-    color: colors.common.white,
-    title: "History",
-    subtitle:
-      "Look through consignment history and view a timeline of events, filtering by date to find what you need.",
-  },
+    {
+        icon: "fas fa-file-invoice",
+        color: colors.common.white,
+        title: "Reporting",
+        subtitle:
+            "Statistics and reporting features, with the ability to export your data."
+    },
+    {
+        icon: "fas fa-motorcycle",
+        color: colors.common.white,
+        title: "Fleet management",
+        subtitle: "Add your vehicles and assign riders to them."
+    },
+    {
+        icon: "fas fa-mobile-alt",
+        color: colors.common.white,
+        title: "Use anywhere",
+        subtitle:
+            "Developed with standard web technology for compatibility across a broad range of devices."
+    },
+    {
+        icon: "fa-solid fa-hand-pointer",
+        color: colors.common.white,
+        title: "Easy to use",
+        subtitle:
+            "Used by volunteers across the country, Platelet Dispatch lets them save lives without slowing them down."
+    },
+    {
+        icon: "fas fa-rocket",
+        color: colors.common.white,
+        title: "Update and track consignment status",
+        subtitle:
+            "Mark consignments as Active, Picked Up, Delivered, Rider Home, or Cancelled, Rejected and Abandoned."
+    },
+    {
+        icon: "fa-solid fa-clock-rotate-left",
+        color: colors.common.white,
+        title: "History",
+        subtitle:
+            "Look through consignment history and view a timeline of events, filtering by date to find what you need."
+    }
 ];
 
 const Features = ({ className, ...rest }) => {
-  const classes = useStyles();
-  const theme = useTheme();
+    const classes = useStyles();
 
-  return (
-    <div className={className} {...rest}>
-      <SectionHeader
-        title={""}
-        fadeUp
-        titleProps={{
-          variant: "h3",
-          color: "textPrimary",
-          className: classes.fontWeight900,
-        }}
-      />
-      <Grid container spacing={2}>
-        {data.map((adv, index) => (
-          <Grid
-            key={index}
-            item
-            container
-            alignItems="center"
-            direction="column"
-            xs={6}
-            md={4}
-            data-aos="fade-up"
-          >
-            <CardBase
-              liftUp
-              variant="outlined"
-              style={{
-                borderRadius: "1em",
-              }}
-            >
-              <DescriptionListIcon
-                icon={
-                  <IconAlternate
-                    fontIconClass={adv.icon}
-                    color={adv.color}
-                    shape="circle"
-                    size="small"
-                  />
-                }
-                title={adv.title}
-                subtitle={adv.subtitle}
-                align="left"
-              />
-            </CardBase>
-          </Grid>
-        ))}
-      </Grid>
-    </div>
-  );
+    return (
+        <div className={className} {...rest}>
+            <SectionHeader
+                title={""}
+                fadeUp
+                titleProps={{
+                    variant: "h3",
+                    color: "textPrimary",
+                    className: classes.fontWeight900
+                }}
+            />
+            <Grid container spacing={2}>
+                {data.map((adv, index) => (
+                    <Grid
+                        key={index}
+                        item
+                        container
+                        alignItems="center"
+                        direction="column"
+                        xs={6}
+                        md={4}
+                        data-aos="fade-up"
+                    >
+                        <CardBase
+                            liftUp
+                            variant="outlined"
+                            style={{
+                                borderRadius: "1em"
+                            }}
+                        >
+                            <DescriptionListIcon
+                                icon={
+                                    <IconAlternate
+                                        fontIconClass={adv.icon}
+                                        color={adv.color}
+                                        shape="circle"
+                                        size="small"
+                                    />
+                                }
+                                title={adv.title}
+                                subtitle={adv.subtitle}
+                                align="left"
+                            />
+                        </CardBase>
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    );
 };
 
 Features.propTypes = {
-  /**
-   * External classes
-   */
-  className: PropTypes.string,
+    /**
+     * External classes
+     */
+    className: PropTypes.string
 };
 
 export default Features;
--- cdk-platform/lib/hosting/landing-page/src/views/IndexView/components/Services/Sevices.js
+++ cdk-platform/lib/hosting/landing-page/src/views/IndexView/components/Services/Sevices.js
@@ -1,66 +1,46 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {useTheme, makeStyles} from '@material-ui/core/styles';
-import {useMediaQuery, Grid, Typography, colors} from '@material-ui/core';
-import {IconAlternate, SectionHeader} from 'components/molecules';
-import {DescriptionListIcon, Section} from 'components/organisms';
-
-const useStyles = makeStyles(() => ({
-    fontWeight900: {
-        fontWeight: 900,
-    },
-    noPaddingBottom: {
-        paddingBottom: 0,
-    },
-    noPaddingTop: {
-        paddingTop: 0,
-    },
-    paddingLeft: {
-        paddingTop: 0,
-        paddingLeft: "100px"
-    },
-}));
+import React from "react";
+import PropTypes from "prop-types";
+import { Grid, colors } from "@material-ui/core";
+import { IconAlternate } from "components/molecules";
+import { DescriptionListIcon } from "components/organisms";
 
 const dataRow1 = [
     {
-        icon: 'fab fa-sketch',
-        title: 'Real time updates',
+        icon: "fab fa-sketch",
+        title: "Real time updates",
         subtitle:
-            'Assign jobs in real time and be notified on pick ups and deliveries.',
+            "Assign jobs in real time and be notified on pick ups and deliveries."
     },
     {
-        icon: 'fas fa-code',
-        title: 'Directory of locations',
-        subtitle:
-            "Quickly select locations from a searchable directory.",
-    },
+        icon: "fas fa-code",
+        title: "Directory of locations",
+        subtitle: "Quickly select locations from a searchable directory."
+    }
 ];
 const dataRow2 = [
     {
-        icon: 'fab fa-sketch',
-        title: 'Record details',
+        icon: "fab fa-sketch",
+        title: "Record details",
         subtitle:
-            'Couriers can use a mobile device to record names on pick up and delivery.',
+            "Couriers can use a mobile device to record names on pick up and delivery."
     },
     {
-        icon: 'fas fa-code',
-        title: 'Assign vehicles',
-        subtitle:
-            "Assign users to vehicles that are a part of your fleet.",
-    },
+        icon: "fas fa-code",
+        title: "Assign vehicles",
+        subtitle: "Assign users to vehicles that are a part of your fleet."
+    }
 ];
-const Services = ({className, ...rest}) => {
-    const classes = useStyles();
-    const theme = useTheme();
-    const isMd = useMediaQuery(theme.breakpoints.up('md'), {
-        defaultMatches: true,
-    });
-
+const Services = ({ className }) => {
     return (
         <Grid className={className} direction={"column"} container spacing={2}>
             <Grid container direction={"row"} item spacing={2}>
                 {dataRow1.map((item, index) => (
-                    <Grid key={index} item style={{width: 300}} data-aos={'fade-up'}>
+                    <Grid
+                        key={index}
+                        item
+                        style={{ width: 300 }}
+                        data-aos={"fade-up"}
+                    >
                         <DescriptionListIcon
                             title={item.title}
                             subtitle={item.subtitle}
@@ -78,7 +58,12 @@ const Services = ({className, ...rest}) => {
             </Grid>
             <Grid container direction={"row"} item spacing={2}>
                 {dataRow2.map((item, index) => (
-                    <Grid key={index} item style={{width: 300}} data-aos={'fade-up'}>
+                    <Grid
+                        key={index}
+                        item
+                        style={{ width: 300 }}
+                        data-aos={"fade-up"}
+                    >
                         <DescriptionListIcon
                             title={item.title}
                             subtitle={item.subtitle}
@@ -102,7 +87,7 @@ Services.propTypes = {
     /**
      * External classes
      */
-    className: PropTypes.string,
+    className: PropTypes.string
 };
 
 export default Services;
