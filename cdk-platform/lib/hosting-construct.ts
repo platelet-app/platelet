d919d6c61992490e832306817df58a897be9d214
Theo Cranmore <theo.cranmore@gmail.com>
2026-06-11 08:11:29 +0100

    feat/252: hosting construct start

---
+++ cdk-platform/lib/hosting-construct.ts
@@ -0,0 +1,20 @@
+import {Construct} from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import {BucketDeployment, Source} from "aws-cdk-lib/aws-s3-deployment";
+
+export interface HostingConstructProps {
+    region: string;
+}
+
+export class HostingConstruct extends Construct {
+    constructor(scope: Construct, id: string, props: HostingConstructProps) {
+        super(scope, id);
+        const landingBucket = new s3.Bucket(this, "PlateletLandingPage", {
+            accessControl: s3.BucketAccessControl.PRIVATE,
+        })
+        new BucketDeployment(this, 'BucketDeployment', {
+            destinationBucket: landingBucket,
+            sources: [Source.asset('../platelet-landing/build')]
+        })
+    }
+}
