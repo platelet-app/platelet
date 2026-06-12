710e0717d79b1a984bc5841b85ec9a8bd88242b8
Theo Cranmore <theo.cranmore@gmail.com>
2026-06-12 07:36:14 +0100

    feat/252: more work on landing page and distribution

--- cdk-platform/lib/hosting-construct.ts
+++ cdk-platform/lib/hosting-construct.ts
@@ -1,9 +1,13 @@
-import { Construct } from "constructs";
-import { Distribution, OriginAccessIdentity } from "@aws-cdk/aws-cloudfront";
-import { S3Origin } from "@aws-cdk/aws-cloudfront-origins";
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as cloudfront_origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+
+import {CfnOutput, Duration, RemovalPolicy} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
 import * as s3 from "aws-cdk-lib/aws-s3";
-import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
-import { RemovalPolicy } from "aws-cdk-lib";
 
 export interface HostingConstructProps {
     region: string;
@@ -12,26 +16,84 @@ export interface HostingConstructProps {
 export class HostingConstruct extends Construct {
     constructor(scope: Construct, id: string, props: HostingConstructProps) {
         super(scope, id);
-        const landingBucket = new s3.Bucket(this, "PlateletLandingPage", {
-            accessControl: s3.BucketAccessControl.PRIVATE,
+        // 1. Define the domain name by changing'stormit.link'.
+        const domainName = 'platelet.app';
+        const siteDomain = `platform-landing.${domainName}`;
+
+        // 1.1 Create a Route 53 hosted zone (optional - you will need to update the NS records).
+        /*
+        const hostedZone = new route53.PublicHostedZone(this, 'MyHostedZone', {
+            zoneName: domainName,
+            });
+        new CfnOutput(this, 'Site', { value: 'https://' + siteDomain });
+        */
+
+        // 1.2 Find the current hosted zone in Route 53
+        const zone = route53.HostedZone.fromLookup(this, 'Zone', {domainName: domainName});
+        console.log(zone);
+
+        // 2. Create a TLS/SSL certificate for HTTPS
+        const certificate = new acm.Certificate(this, 'PlatformHostingCertificate', {
+            domainName: domainName,
+            subjectAlternativeNames: [`*.${domainName}`],
+            validation: acm.CertificateValidation.fromDns(zone),
+        });
+
+        // 2.1 The removal policy for the certificate can be set to 'Retain' or 'Destroy'
+        certificate.applyRemovalPolicy(RemovalPolicy.DESTROY)
+        new CfnOutput(this, 'PlatformCertificate', {value: certificate.certificateArn});
+
+        // 3. Create an S3 bucket to store content, and set the removal policy to either 'Retain' or 'Destroy'
+        // Please be aware that all content stored in the S3 bucket is publicly available.
+        const siteBucket = new s3.Bucket(this, 'LandingPage', {
+            bucketName: siteDomain,
             removalPolicy: RemovalPolicy.DESTROY,
-        });
-        new BucketDeployment(this, "BucketDeployment", {
-            destinationBucket: landingBucket,
-            sources: [Source.asset("./lib/hosting/landing-page/build")],
-        });
+            autoDeleteObjects: true,
+        })
+        new CfnOutput(this, 'LandingPageBucket', {value: siteBucket.bucketName});
 
-        const originAccessIdentity = new OriginAccessIdentity(
-            this,
-            "LandingOriginAccessIdentity"
-        );
-        landingBucket.grantRead(originAccessIdentity);
-
-        new Distribution(this, "Platelet.AppDistribution", {
+        // 4. Deploy CloudFront distribution
+        const distribution = new cloudfront.Distribution(this, 'PlatformDistribution', {
+            certificate: certificate,
             defaultRootObject: "index.html",
+            domainNames: [siteDomain, domainName],
+            minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+            errorResponses: [
+                {
+                    httpStatus: 404,
+                    responseHttpStatus: 404,
+                    responsePagePath: '/index.html',
+                    ttl: Duration.minutes(30),
+                }
+            ],
             defaultBehavior: {
-                origin: new S3Origin(landingBucket, { originAccessIdentity }),
-            },
+                origin: cloudfront_origins.S3BucketOrigin.withOriginAccessControl(siteBucket),
+                compress: true,
+                allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+                viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            }
+        });
+
+        new CfnOutput(this, 'DistributionId', {value: distribution.distributionId});
+
+        // 5. Create a Route 53 alias record for the CloudFront distribution
+        //5.1  Add an 'A' record to Route 53 for 'www.example.com'
+        //  new route53.ARecord(this, 'WWWSiteAliasRecord', {
+        //      zone,
+        //      recordName: siteDomain,
+        //      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution))
+        //  });
+        //  //5.2 Add an 'A' record to Route 53 for 'example.com'
+        //  new route53.ARecord(this, 'SiteAliasRecord', {
+        //      zone,
+        //      recordName: domainName,
+        //      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution))
+        //  });
+
+        //6. Deploy the files from the 'html-website' folder to an S3 bucket
+        new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+            sources: [s3deploy.Source.asset('./lib/hosting/landing-page')],
+            destinationBucket: siteBucket,
         });
     }
 }
