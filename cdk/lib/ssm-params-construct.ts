9d851f443ca57c84364978abde07bf5fd5498319
Theo Cranmore <theo.cranmore@gmail.com>
2026-07-15 14:35:29 +0100

    feat/265: cdk output from email value

--- cdk/lib/ssm-params-construct.ts
+++ cdk/lib/ssm-params-construct.ts
@@ -13,23 +13,34 @@ export class SSMParamsConstruct extends Construct {
     constructor(scope: Construct, id: string, props: SSMParamsConstructProps) {
         super(scope, id);
 
-        const fromEmail = new ssm.StringParameter(this, "FromEmailSSMParam", {
-            parameterName: `/platelet-supporting-cdk/${props.amplifyEnv}/FromEmail`,
-            stringValue: props.fromEmail,
-        });
-        const domainName = new ssm.StringParameter(this, "DomainNameSSMParam", {
-            parameterName: `/platelet-supporting-cdk/${props.amplifyEnv}/DomainName`,
-            stringValue: props.domainName,
-        });
+        const fromEmailParam = new ssm.StringParameter(
+            this,
+            "FromEmailSSMParam",
+            {
+                parameterName: `/platelet-supporting-cdk/${props.amplifyEnv}/FromEmail`,
+                stringValue: props.fromEmail,
+            }
+        );
+        const domainNameParam = new ssm.StringParameter(
+            this,
+            "DomainNameSSMParam",
+            {
+                parameterName: `/platelet-supporting-cdk/${props.amplifyEnv}/DomainName`,
+                stringValue: props.domainName,
+            }
+        );
 
-        this.fromEmailArn = fromEmail.parameterArn;
+        this.fromEmailArn = fromEmailParam.parameterArn;
 
         // output values to be used in custom-policies.json files
         new cdk.CfnOutput(this, "FromEmailSSMParamArnOutput", {
             value: this.fromEmailArn,
         });
         new cdk.CfnOutput(this, "DomainNameSSMParamArnOutput", {
-            value: domainName.parameterArn,
+            value: domainNameParam.parameterArn,
+        });
+        new cdk.CfnOutput(this, "FromEmailValueOutput", {
+            value: props.fromEmail,
         });
     }
 }
