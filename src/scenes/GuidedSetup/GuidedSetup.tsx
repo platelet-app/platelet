00eb69e3116235abd7ec7924720c71fed24e5c6a
Theo Cranmore <theo.cranmore@gmail.com>
2026-07-15 09:22:12 +0100

    put task in future if drop off also due more than a day later, unless pick up is due sooner

--- src/scenes/GuidedSetup/GuidedSetup.tsx
+++ src/scenes/GuidedSetup/GuidedSetup.tsx
@@ -207,23 +207,40 @@ export const GuidedSetup = () => {
     const whoami = useSelector(getWhoami);
 
     useEffect(() => {
-        if (!schedule.pickUp) {
+        if (!schedule.pickUp && !schedule.dropOff) {
             setFutureOrDash(null);
             return;
         }
-        if (futureOrDash) return;
         if (schedule.pickUp) {
             const convertedSchedule = convertScheduleToTaskData(
                 schedule.pickUp
             );
-            const isDueInOneDay = !taskScheduleDueStatus(
+            const isDueInMoreThanOneDay = !taskScheduleDueStatus(
                 convertedSchedule,
                 0,
                 1
             );
-            if (isDueInOneDay) setFutureOrDash("future");
+            if (isDueInMoreThanOneDay) {
+                setFutureOrDash("future");
+            } else {
+                setFutureOrDash(null);
+            }
+        } else if (schedule.dropOff) {
+            const convertedSchedule = convertScheduleToTaskData(
+                schedule.dropOff
+            );
+            const isDueInMoreThanOneDay = !taskScheduleDueStatus(
+                convertedSchedule,
+                0,
+                1
+            );
+            if (isDueInMoreThanOneDay) {
+                setFutureOrDash("future");
+            } else {
+                setFutureOrDash(null);
+            }
         }
-    }, [schedule.pickUp, futureOrDash]);
+    }, [schedule]);
 
     const handleChange = (_: any, newValue: number) => {
         setTabIndex(newValue);
