5f5453a48ccc59fc0bb81fd6b32afd61d5bbd9cf
Theo Cranmore <theo.cranmore@gmail.com>
2026-08-04 08:41:50 +0100

    feat/269: skip stack env and get from credentials

--- cdk/bin/platelet-supporting-cdk.ts
+++ cdk/bin/platelet-supporting-cdk.ts
@@ -7,12 +7,8 @@ const app = new cdk.App();
 
 const deployEnv = process.env.DEPLOY_ENV || "dev";
 
-const awsAccountNumber = process.env.AWS_ACCOUNT_ID;
-
 const stackName = deployEnv.replace(/[^A-Za-z0-9-]/g, "-");
 
-new PlateletCdkStack(app, `PlateletCdkStack-${stackName}`, {
-    env: { account: awsAccountNumber, region: "eu-west-1" },
-});
+new PlateletCdkStack(app, `PlateletCdkStack-${stackName}`, {});
 
 cdk.Aspects.of(app).add(new AwsSolutionsChecks());
