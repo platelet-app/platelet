ddcba8522f7118e6ceb0087490dfec8c1cb3a9cd
Theo Cranmore <theo.cranmore@gmail.com>
2026-08-04 08:50:16 +0100

    feat/269: default region to eu-west-1 if not defined

--- cdk/bin/platelet-supporting-cdk.ts
+++ cdk/bin/platelet-supporting-cdk.ts
@@ -7,8 +7,12 @@ const app = new cdk.App();
 
 const deployEnv = process.env.DEPLOY_ENV || "dev";
 
+const region = process.env.AWS_REGION || "eu-west-1";
+
 const stackName = deployEnv.replace(/[^A-Za-z0-9-]/g, "-");
 
-new PlateletCdkStack(app, `PlateletCdkStack-${stackName}`, {});
+new PlateletCdkStack(app, `PlateletCdkStack-${stackName}`, {
+    env: { region },
+});
 
 cdk.Aspects.of(app).add(new AwsSolutionsChecks());
